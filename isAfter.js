b7702d82402dd6347d80fe1c59cee7674fe70557
Steve Winberg <stevewin21@gmail.com>
2018-08-19 19:09:37 +0700

    refactor isAfter for readability

--- isAfter.js
+++ isAfter.js
@@ -1,24 +1,23 @@
-(function() {
-  var debug, isAfter, isDate, isFloat, moment;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isAfter');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isAfter');
+  const moment = require('moment');
+  const isFloat = require('./isFloat');
+  const isDate = require('./isDate');
 
-  moment = require('moment');
-
-  isFloat = require('./isFloat');
-
-  isDate = require('./isDate');
-
-  isAfter = function(value, compare = Date.now()) {
+  const isAfter = (value, compare = Date.now()) => {
     debug('call:isAfter(%o, %o)', value, compare);
+
     if (isFloat(value) && isFloat(compare)) {
       return parseFloat(value) > parseFloat(compare);
     } else if (isDate(value)) {
       return moment(value).isAfter(compare);
     }
+
     return false;
   };
 
   module.exports = isAfter;
-
-}).call(this);
+})(); // end IIFE
