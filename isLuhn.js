e5ac1e4be150cbbe1f727a9cae7116f685f93708
Steve Winberg <stevewin21@gmail.com>
2018-08-19 21:06:31 +0700

    refactor isLuhn for readability

--- isLuhn.js
+++ isLuhn.js
@@ -1,30 +1,44 @@
-(function() {
-  var DOUBLE, REGEX, _, debug, isLuhn;
+'use strict';
 
-  _ = require('underscore');
+(() => {
+  // include dependencies
+  const _ = require('underscore');
+  const debug = require('debug')('@scuba-squad:validation:isLuhn');
+  const REGEX = /^\d+$/;
+  const DOUBLE = [
+    0,
+    2,
+    4,
+    6,
+    8,
+    1,
+    3,
+    5,
+    7,
+    9,
+  ];
 
-  debug = require('debug')('@scuba-squad:validation:isLuhn');
-
-  REGEX = /^\d+$/;
-
-  DOUBLE = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
-
-  isLuhn = function(value) {
-    var sum;
+  const isLuhn = (value) => {
     debug('call:isLuhn(%o)', value);
+
     if (!REGEX.test(value)) {
       return false;
     }
-    sum = _.reduce(value != null ? typeof value.toString === "function" ? value.toString().split('').reverse() : void 0 : void 0, function(memo, val, idx) {
+
+    value = (value && (value.toString() || `${value}`).split('').reverse()) || undefined;
+
+    const sum = _.reduce(value, (memo, val, idx) => {
       val = parseInt(val);
+
       if (parseInt(idx) % 2) {
         return memo + DOUBLE[val];
       }
+
       return memo + val;
     }, 0);
+
     return !(sum % 10);
   };
 
   module.exports = isLuhn;
-
-}).call(this);
+})(); // end IIFE
