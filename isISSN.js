82e998805fd531a4ca965e3f0495d100fa7f6777
Steve Winberg <stevewin21@gmail.com>
2018-08-19 20:38:39 +0700

    refactor isISSN for readability

--- isISSN.js
+++ isISSN.js
@@ -1,29 +1,33 @@
-(function() {
-  var REGEX, debug, isISSN;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isISSN');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isISSN');
+  const REGEX = /^\d{4}-?\d{3}[\dX]$/i;
 
-  REGEX = /^\d{4}-?\d{3}[\dX]$/i;
-
-  isISSN = function(value) {
-    var i, idx, len, sum, val;
+  const isISSN = (value) => {
     debug('call:isISSN(%o)', value);
+
+    let sum = 0;
+
     if (!REGEX.test(value)) {
       return false;
     }
+
     value = value.replace(/[^\dX]/gi, '').split('');
-    sum = 0;
+
     if (/^x$/i.test(value[7])) {
       value[7] = 10;
     }
-    for (idx = i = 0, len = value.length; i < len; idx = ++i) {
-      val = value[idx];
-      val = parseInt(val);
+
+    for (let idx = 0; idx < value.length; idx++) {
+      const val = parseInt(value[idx]);
+
       sum += (8 - idx) * val;
     }
+
     return !(sum % 11);
   };
 
   module.exports = isISSN;
-
-}).call(this);
+})(); // end IIFE
