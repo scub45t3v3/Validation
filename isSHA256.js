117b8197b52af57a738ef2170e4af26d9af6dfc6
Steve Winberg <stevewin21@gmail.com>
2018-08-19 22:00:36 +0700

    refactor isSHA256 for readability

--- isSHA256.js
+++ isSHA256.js
@@ -1,15 +1,15 @@
-(function() {
-  var debug, isAll, isSHA256;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isSHA256');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isSHA256');
+  const isAll = require('./isAll');
 
-  isAll = require('./isAll');
-
-  isSHA256 = function(value) {
+  const isSHA256 = (value) => {
     debug('call:isSHA256(%o)', value);
+
     return isAll(value, 'isHexadecimal', ['isLength', 64]);
   };
 
   module.exports = isSHA256;
-
-}).call(this);
+})(); // end IIFE
