bb993f172247ea5b48dc5d5ae005ca9909b03280
Steve Winberg <stevewin21@gmail.com>
2018-08-19 22:43:08 +0700

    refactor isURL for readability

--- isURL.js
+++ isURL.js
@@ -1,27 +1,23 @@
-(function() {
-  var REGEX, debug, isDomainName, isIP, isURL;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isURL');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isURL');
+  const isDomainName = require('./isDomainName');
+  const isIP = require('./isIP');
+  const REGEX = /^[a-z][a-z\d+.-]+:\/\/(?:(?:[\w!$&'()*+,;:=~.-]|%[\da-f]{2})+@)?([^\s:[\]/%?#@]+|\[[:.%\w]+\])(?::\d+)?(?:\/(?:[\w!$&'()*+,;:@=~.-]|%[\da-f]{2})+)*\/?(?:\?(?:[\w!$&'()*+,;:@=~?/.-]|%[\da-f]{2})*)?(?:#(?:[\w!$&'()*+,;:@=~?/.-]|%[\da-f]{2})*)?$/i;
 
-  isDomainName = require('./isDomainName');
-
-  isIP = require('./isIP');
-
-  REGEX = /^[a-z][a-z\d+.-]+:\/\/(?:(?:[\w!$&'()*+,;:=~.-]|\%[\da-f]{2})+@)?([^\s:\[\]\/%?#@]+|\[[:.%\w]+\])(?::\d+)?(?:\/(?:[\w!$&'()*+,;:@=~.-]|\%[\da-f]{2})+)*\/?(?:\?(?:[\w!$&'()*+,;:@=~?\/.-]|\%[\da-f]{2})*)?(?:\#(?:[\w!$&'()*+,;:@=~?\/.-]|\%[\da-f]{2})*)?$/i; // protocol
-  // user
-  // domain|ip
-  // port
-  // path
-  // query
-  // fragment
-
-  isURL = function(value) {
-    var ref, ref1;
+  const isURL = (value) => {
     debug('call:isURL(%o)', value);
-    value = value != null ? typeof value.match === "function" ? (ref = value.match(REGEX)) != null ? (ref1 = ref[1]) != null ? ref1.replace(/^\[|\]$/g, '') : void 0 : void 0 : void 0 : void 0;
-    return !!value && (isDomainName(value) || isIP(value));
+
+    try {
+      value = value.match(REGEX)[1].replace(/^\[|\]$/g, '');
+
+      return isDomainName(value) || isIP(value);
+    } catch (error) {
+      return false;
+    }
   };
 
   module.exports = isURL;
-
-}).call(this);
+})(); // end IIFE
