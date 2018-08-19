89bf2cad51f7e0df8bd06bc951f0b31971aa25be
Steve Winberg <stevewin21@gmail.com>
2018-08-19 19:57:29 +0700

    refactor isEmailAddress for readability

--- isEmailAddress.js
+++ isEmailAddress.js
@@ -1,11 +1,11 @@
-(function() {
-  var REGEX, debug, isDomainName, isEmailAddress, isLength;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isEmailAddress');
-
-  isLength = require('./isLength');
-
-  isDomainName = require('./isDomainName');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isEmailAddress');
+  const isLength = require('./isLength');
+  const isDomainName = require('./isDomainName');
+  const REGEX = /^((?:[\w!#$%&'*+/=?^`{|}~-]+\.?)*[\w!#$%&'*+/=?^`{|}~-]+)@([^\s:[\]/%?#@]+)$/i;
 
   /*
    * The following method validates the local-part to dot-atom RFC 5322
@@ -13,32 +13,35 @@
    * It validates the domain to dot-atom RFC 5322 with RFC 5890 IDN support
    * thus removing the domain-literal, obs-domain and comments.
    *
-   * Since everyone seems to have a different interpretation on what a valid email
-   * address is or is not. I am not trying to validate to a specific specification
-   * as the RFC specs change, discourage, deprecate and leave some ambiguity in
-   * their interpretation and at the end of the day what really matters is what
-   * the various email service providers support and interpret.
+   * Since everyone seems to have a different interpretation on what a valid
+   * email address is or is not. I am not trying to validate to a specific
+   * specification as the RFC specs change, discourage, deprecate and leave
+   * some ambiguity in their interpretation and at the end of the day what
+   * really matters is what the various email service providers support and
+   * interpret.
    */
-  REGEX = /^((?:[\w!#$%&'*+\/=?^`{|}~-]+\.?)*[\w!#$%&'*+\/=?^`{|}~-]+)@([^\s:\[\]\/%?#@]+)$/i; // username
-  // @ seperator
-  // domain
-
-  isEmailAddress = function(value) {
-    var domain, username;
+  const isEmailAddress = (value) => {
     debug('call:isEmailAddress(%o)', value);
-    value = value != null ? typeof value.match === "function" ? value.match(REGEX) : void 0 : void 0;
-    domain = isDomainName(value != null ? value[2] : void 0, true);
-    username = isLength(value != null ? value[1] : void 0, {
+
+    try {
+      value = value.match(REGEX);
+    } catch (error) {
+      return false;
+    }
+
+    const domain = isDomainName(value && value[2], true);
+    const username = isLength(value && value[1], {
       min: 1,
-      max: 64
+      max: 64,
     });
-    value = isLength(value != null ? value[0] : void 0, {
+
+    value = isLength(value && value[0], {
       min: 6,
-      max: 255
+      max: 255,
     });
+
     return value && username && domain;
   };
 
   module.exports = isEmailAddress;
-
-}).call(this);
+})(); // end IIFE
