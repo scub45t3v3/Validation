74e49272bcfa62af930f2471991ce2d2cb3748fe
Steve Winberg <stevewin21@gmail.com>
2018-08-19 21:31:37 +0700

    refactor isPhoneNumber for readability

--- isPhoneNumber.js
+++ isPhoneNumber.js
@@ -1,40 +1,42 @@
-(function() {
-  var Country, PhoneNumberUtil, debug, isPhoneNumber, isString;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isPhoneNumber');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isPhoneNumber');
+  const {PhoneNumberUtil} = require('google-libphonenumber');
+  const Country = require('@scuba-squad/country');
+  const isString = require('./isString');
 
-  ({PhoneNumberUtil} = require('google-libphonenumber'));
-
-  Country = require('@scuba-squad/country');
-
-  isString = require('./isString');
-
-  isPhoneNumber = function(value, country) {
-    var error, iso, parsed, ref, util;
+  const isPhoneNumber = (value, country) => {
     debug('call:isPhoneNumber(%o, %o)', value, country);
-    util = PhoneNumberUtil.getInstance();
+
+    const util = PhoneNumberUtil.getInstance();
+
     if (isString(country)) {
-      iso = country != null ? typeof country.trim === "function" ? (ref = country.trim()) != null ? typeof ref.toUpperCase === "function" ? ref.toUpperCase() : void 0 : void 0 : void 0 : void 0;
+      const iso = (country && country.trim().toUpperCase()) || undefined;
+
       country = Country.getByIso2Code(iso);
       country || (country = Country.getByIso3Code(iso));
       country || (country = Country.getByIsoNumericCode(iso));
     }
+
     try {
-      iso = country != null ? country.iso2Code : void 0;
-      parsed = util.parse(value, iso);
+      const iso = (country && country.iso2Code) || undefined;
+      const parsed = util.parse(value, iso);
+
       if (!util.isValidNumber(parsed)) {
         return false;
       }
+
       if (iso && util.getRegionCodeForNumber(parsed) !== iso) {
         return false;
       }
+
       return true;
-    } catch (error1) {
-      error = error1;
+    } catch (error) {
       return false;
     }
   };
 
   module.exports = isPhoneNumber;
-
-}).call(this);
+})(); // end IIFE
