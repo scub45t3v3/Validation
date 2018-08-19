7086f97c6d4e5cbe835cabc63da271b5c56425a7
Steve Winberg <stevewin21@gmail.com>
2018-08-19 20:31:00 +0700

    refactor isISBN for readability

--- isISBN.js
+++ isISBN.js
@@ -1,17 +1,17 @@
-(function() {
-  var REGEX_10, REGEX_13, debug, isISBN, isISBN10, isISBN13;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isISBN');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isISBN');
+  const REGEX_10 = /^\d{9}[\dX]$/;
+  const REGEX_13 = /^\d{13}$/;
 
-  REGEX_10 = /^\d{9}[\dX]$/;
-
-  REGEX_13 = /^\d{13}$/;
-
-  isISBN = function(value, version) {
-    var ref, sanitized;
+  const isISBN = (value, version) => {
     debug('call:isISBN(%o, %o)', value, version);
-    sanitized = value != null ? typeof value.toString === "function" ? (ref = value.toString()) != null ? typeof ref.toUpperCase === "function" ? ref.toUpperCase().replace(/[\s-]+/g, '') : void 0 : void 0 : void 0 : void 0;
-    version || (version = sanitized != null ? sanitized.length : void 0);
+
+    const sanitized = (value && (value.toString() || `${value}`).toUpperCase().replace(/[\s-]+/g, '')) || undefined;
+    version || (version = (sanitized && sanitized.length) || undefined);
+
     switch (version) {
       case 10:
       case '10':
@@ -22,45 +22,56 @@
       default:
         return false;
     }
-  };
+  }; // end ISISBN
 
-  isISBN10 = function(value) {
-    var i, idx, len, sum, val;
+  const isISBN10 = (value) => {
     debug('call:isISBN10(%o)', value);
+
+    let sum = 0;
+
     if (!REGEX_10.test(value)) {
       return false;
     }
-    sum = 0;
+
     value = value.split('').reverse();
+
     if (value[0] === 'X') {
       value[0] = 10;
     }
-    for (idx = i = 0, len = value.length; i < len; idx = ++i) {
-      val = value[idx];
+
+    for (let idx = 0; idx < value.length; idx++) {
+      const val = value[idx];
+
       sum += (idx + 1) * parseInt(val);
     }
-    return !(sum % 11);
-  };
 
-  isISBN13 = function(value) {
-    var i, idx, len, ref, sum, val;
+    return !(sum % 11);
+  }; // end ISISBN10
+
+  const isISBN13 = (value) => {
     debug('call:isISBN13(%o)', value);
+
+    let sum = 0;
+
     if (!REGEX_13.test(value)) {
       return false;
     }
-    sum = 0;
-    ref = value.split('');
-    for (idx = i = 0, len = ref.length; i < len; idx = ++i) {
-      val = ref[idx];
-      val = parseInt(val);
+
+    value = value.split('');
+
+    for (let idx = 0; idx < value.length; idx++) {
+      let val = parseInt(value[idx]);
+
       if (idx % 2) {
         val *= 3;
       }
+
       sum += val;
     }
+
     return !(sum % 10);
-  };
+  }; // end isISBN13
 
+  // export as commonjs module
   module.exports = isISBN;
-
-}).call(this);
+})(); // end IIFE
