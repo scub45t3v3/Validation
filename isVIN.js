b352671b87230e8c524998a3715bda40588f4cc0
Steve Winberg <stevewin21@gmail.com>
2018-08-19 22:58:27 +0700

    refactor isVIN for readability

--- isVIN.js
+++ isVIN.js
@@ -1,38 +1,46 @@
-(function() {
-  var REGEX, debug, getCheckDigit, isVIN, transliterate;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isVIN');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isVIN');
+  const REGEX = /^[a-hj-nprs-z\d]{17}$/i;
 
-  REGEX = /^[a-hj-nprs-z\d]{17}$/i;
-
-  isVIN = function(value) {
+  const isVIN = (value) => {
     debug('call:isVIN(%o)', value);
+
     if (!REGEX.test(value)) {
       return false;
     }
+
     return getCheckDigit(value) === value[8].toUpperCase();
   };
 
-  transliterate = function(char) {
+  const transliterate = (char) => {
     debug('call:transliterate(%o)', char);
-    char = char != null ? char.toUpperCase() : void 0;
+
+    char = (char && char.toUpperCase()) || undefined;
+
     return '0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ'.indexOf(char) % 10;
   };
 
-  getCheckDigit = function(value) {
-    var char, i, idx, len, map, ref, sum, weights;
+  const getCheckDigit = (value) => {
     debug('call:getCheckDigit(%o)', value);
-    map = '0123456789X';
-    weights = '8765432X098765432';
-    sum = 0;
-    ref = value.toUpperCase().split('');
-    for (idx = i = 0, len = ref.length; i < len; idx = ++i) {
-      char = ref[idx];
+
+    const map = '0123456789X';
+    const weights = '8765432X098765432';
+
+    let sum = 0;
+
+    value = value.toUpperCase().split('');
+
+    for (let idx = 0; idx < value.length; idx++) {
+      const char = value[idx];
+
       sum += transliterate(char) * map.indexOf(weights[idx]);
     }
+
     return map[sum % 11];
   };
 
   module.exports = isVIN;
-
-}).call(this);
+})(); // end IIFE
