61b40b747b227f7ff680b24758864ac30737481d
Steve Winberg <stevewin21@gmail.com>
2018-08-19 22:48:15 +0700

    refactor isURN for readability

--- isURN.js
+++ isURN.js
@@ -1,19 +1,15 @@
-(function() {
-  var REGEX, debug, isURN;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isURN');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isURN');
+  const REGEX = /^urn:([a-z\d][a-z\d-]{0,30}[a-z\d]):((?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@/]|%[\da-fA-F]{2})*)(\?\+(?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@/?]|%[\da-fA-F]{2})*)?(\?=(?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@/?]|%[\da-fA-F]{2})*)?(#(?:[\w.\-~!$&'()*+,;=':@/?]|%[\da-fA-F]{2})*)?$/i;
 
-  REGEX = /^urn:([a-z\d][a-z\d-]{0,30}[a-z\d]):((?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@\/]|%[\da-fA-F]{2})*)(\?\+(?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@\/?]|%[\da-fA-F]{2})*)?(\?\=(?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@\/?]|%[\da-fA-F]{2})*)?(\#(?:[\w.\-~!$&'()*+,;=':@\/?]|%[\da-fA-F]{2})*)?$/i; // NID
-  // NSS
-  // r-component
-  // q-component
-  // f-component
-
-  isURN = function(value) {
+  const isURN = (value) => {
     debug('call:isURN(%o)', value);
+
     return REGEX.test(value);
   };
 
   module.exports = isURN;
-
-}).call(this);
+})(); // end IIFE
