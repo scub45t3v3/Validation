d57f47a81397dc401a2bb998b7ff7684652e5c1d
Steve Winberg <stevewin21@gmail.com>
2018-08-19 19:37:34 +0700

    refactor isBefore for readability

--- isBefore.js
+++ isBefore.js
@@ -1,24 +1,23 @@
-(function() {
-  var debug, isBefore, isDate, isFloat, moment;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isBefore');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isBefore');
+  const moment = require('moment');
+  const isFloat = require('./isFloat');
+  const isDate = require('./isDate');
 
-  moment = require('moment');
-
-  isFloat = require('./isFloat');
-
-  isDate = require('./isDate');
-
-  isBefore = function(value, compare = Date.now()) {
+  const isBefore = (value, compare = Date.now()) => {
     debug('call:isBefore(%o, %o)', value, compare);
+
     if (isFloat(value) && isFloat(compare)) {
       return parseFloat(value) < parseFloat(compare);
     } else if (isDate(value)) {
       return moment(value).isBefore(compare);
     }
+
     return false;
   };
 
   module.exports = isBefore;
-
-}).call(this);
+})(); // end IIFE
