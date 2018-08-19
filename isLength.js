703c282019e0dcc3f0dc4f08c6b443cee9157ed9
Steve Winberg <stevewin21@gmail.com>
2018-08-19 21:01:11 +0700

    refactor isLength for readability

--- isLength.js
+++ isLength.js
@@ -1,25 +1,31 @@
-(function() {
-  var debug, isInteger, isLength;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isLength');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isLength');
+  const isInteger = require('./isInteger');
 
-  isInteger = require('./isInteger');
-
-  isLength = function(value, opt = {}) {
-    var length;
+  const isLength = (value, opt = {}) => {
     debug('call:isLength(%o, %o)', value, opt);
-    length = (value != null ? value.length : void 0) || (value != null ? value.size : void 0);
+
+    const length = (value && (value.length || value.size)) || undefined;
+
     if (isInteger(opt)) {
       opt = {
         min: opt,
-        max: opt
+        max: opt,
       };
     }
-    opt.min || (opt.min = 1);
-    opt.safe = true;
-    return isInteger(length, opt);
+
+    try {
+      opt.min || (opt.min = 1);
+      opt.safe = true;
+
+      return isInteger(length, opt);
+    } catch (error) {
+      return false;
+    }
   };
 
   module.exports = isLength;
-
-}).call(this);
+})(); // end IIFE
