bf5a694fdc02117102bb305666c78cccf77d4bcb
Steve Winberg <stevewin21@gmail.com>
2018-08-19 21:35:23 +0700

    refactor isPort for readability

--- isPort.js
+++ isPort.js
@@ -1,18 +1,18 @@
-(function() {
-  var debug, isInteger, isPort;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isPort');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isPort');
+  const isInteger = require('./isInteger');
 
-  isInteger = require('./isInteger');
-
-  isPort = function(value) {
+  const isPort = (value) => {
     debug('call:isPort(%o)', value);
+
     return isInteger(value, {
       min: 1,
-      max: 65535
+      max: 65535,
     });
   };
 
   module.exports = isPort;
-
-}).call(this);
+})(); // end IIFE
