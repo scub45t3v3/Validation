a5b881aa976d6630e1db9cfc57c8cae02ab85d6f
Steve Winberg <stevewin21@gmail.com>
2018-08-19 21:50:42 +0700

    refactor isSemVer for readability

--- isSemVer.js
+++ isSemVer.js
@@ -1,17 +1,15 @@
-(function() {
-  var REGEX, debug, isSemVer;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isSemVer');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isSemVer');
+  const REGEX = /^(?:\d+\.){2}\d+(?:-(?:0|[1-9]\d*|\d*[a-z-][a-z\d-]*)(?:\.(?:0|[1-9]\d*|\d*[a-z-][a-z\d-]*))*)?(?:\+[a-z\d-]+(?:\.[a-z\d-]+)*)?$/i;
 
-  REGEX = /^(?:\d+\.){2}\d+(?:-(?:0|[1-9]\d*|\d*[a-z-][a-z\d-]*)(?:\.(?:0|[1-9]\d*|\d*[a-z-][a-z\d-]*))*)?(?:\+[a-z\d-]+(?:\.[a-z\d-]+)*)?$/i; // MAJOR.MINOR.PATCH
-  // pre-release tag
-  // metadata tag
-
-  isSemVer = function(value) {
+  const isSemVer = (value) => {
     debug('call:isSemVer(%o)', value);
+
     return REGEX.test(value);
   };
 
   module.exports = isSemVer;
-
-}).call(this);
+})(); // end IIFE
