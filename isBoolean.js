86c16d54c52b219fce75455e47dd983cc6de0ced
Steve Winberg <stevewin21@gmail.com>
2018-08-19 19:39:32 +0700

    refactor isBoolean for readability

--- isBoolean.js
+++ isBoolean.js
@@ -1,13 +1,14 @@
-(function() {
-  var debug, isBoolean;
+'use strict';
 
-  debug = require('debug')('@scuba-squad:validation:isBoolean');
+(() => {
+  // include dependencies
+  const debug = require('debug')('@scuba-squad:validation:isBoolean');
 
-  isBoolean = function(value) {
+  const isBoolean = (value) => {
     debug('call:isBoolean(%o)', value);
+
     return value instanceof Boolean || value === true || value === false;
   };
 
   module.exports = isBoolean;
-
-}).call(this);
+})(); // end IIFE
