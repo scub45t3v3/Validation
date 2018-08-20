2dab7b2fbfef4e21591522949bcc2b8fb31916c7
Steve Winberg <stevewin21@gmail.com>
2018-08-20 22:05:17 +0700

    refactor test/isEmpty for readability

--- test/isEmpty.js
+++ test/isEmpty.js
@@ -1,55 +1,106 @@
-(function() {
-  var isEmpty, noop, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const {noop} = require('underscore');
+  const isEmpty = require('../isEmpty');
 
-  ({noop} = require('underscore'));
+  // describe #isEmpty
+  describe('#isEmpty', () => {
+    it('should be a function', () => {
+      unit
+        .function(isEmpty);
+    }); // end it
 
-  isEmpty = require('../isEmpty');
+    it('should return true for non iterable/enumerable objects', () => {
+      unit
+        .bool(isEmpty({}))
+        .isTrue()
+        .bool(isEmpty([]))
+        .isTrue()
+        .bool(isEmpty(''))
+        .isTrue();
+    }); // end it
 
-  describe('#isEmpty', function() {
-    it('should be a function', function() {
-      unit.function(isEmpty);
-      return null;
-    });
-    it('should return true for non iterable/enumerable objects', function() {
-      unit.bool(isEmpty({})).isTrue().bool(isEmpty([])).isTrue().bool(isEmpty('')).isTrue();
-      return null;
-    });
-    it('should return true for integers', function() {
-      unit.bool(isEmpty(1)).isTrue().bool(isEmpty(234987)).isTrue().bool(isEmpty(-239874)).isTrue().bool(isEmpty(0)).isTrue();
-      return null;
-    });
-    it('should return true for floats', function() {
-      unit.bool(isEmpty(1.1)).isTrue().bool(isEmpty(-0.4)).isTrue().bool(isEmpty(234.4)).isTrue().bool(isEmpty(54.00000000001)).isTrue();
-      return null;
-    });
-    it('should return true for bools', function() {
-      unit.bool(isEmpty(true)).isTrue().bool(isEmpty(false)).isTrue();
-      return null;
-    });
-    it('should return true for functions', function() {
-      unit.bool(isEmpty(noop)).isTrue().bool(isEmpty(isEmpty)).isTrue();
-      return null;
-    });
-    it('should return true for regexs', function() {
-      unit.bool(isEmpty(/asd/)).isTrue().bool(isEmpty(/\d+/)).isTrue().bool(isEmpty(/1/)).isTrue().bool(isEmpty(new RegExp('3'))).isTrue();
-      return null;
-    });
-    it('should return false for arrays', function() {
-      unit.bool(isEmpty([1, 2])).isFalse().bool(isEmpty(['a', {}, 6])).isFalse();
-      return null;
-    });
-    it('should return false for strings', function() {
-      unit.bool(isEmpty('adsf')).isFalse().bool(isEmpty('34.6')).isFalse().bool(isEmpty('-45fg')).isFalse();
-      return null;
-    });
-    return it('should return false for enumerable objects', function() {
-      unit.bool(isEmpty(new String('asd'))).isFalse().bool(isEmpty({
-        a: 5
-      })).isFalse();
-      return null;
-    });
-  });
+    it('should return true for integers', () => {
+      unit
+        .bool(isEmpty(1))
+        .isTrue()
+        .bool(isEmpty(234987))
+        .isTrue()
+        .bool(isEmpty(-239874))
+        .isTrue()
+        .bool(isEmpty(0))
+        .isTrue();
+    }); // end it
 
-}).call(this);
+    it('should return true for floats', () => {
+      unit
+        .bool(isEmpty(1.1))
+        .isTrue()
+        .bool(isEmpty(-0.4))
+        .isTrue()
+        .bool(isEmpty(234.4))
+        .isTrue()
+        .bool(isEmpty(54.00000000001))
+        .isTrue();
+    }); // end it
+
+    it('should return true for bools', () => {
+      unit
+        .bool(isEmpty(true))
+        .isTrue()
+        .bool(isEmpty(false))
+        .isTrue();
+    }); // end it
+
+    it('should return true for functions', () => {
+      unit
+        .bool(isEmpty(noop))
+        .isTrue()
+        .bool(isEmpty(isEmpty))
+        .isTrue();
+    }); // end it
+
+    it('should return true for regexs', () => {
+      unit
+        .bool(isEmpty(/asd/))
+        .isTrue()
+        .bool(isEmpty(/\d+/))
+        .isTrue()
+        .bool(isEmpty(/1/))
+        .isTrue()
+        .bool(isEmpty(new RegExp('3')))
+        .isTrue();
+    }); // end it
+
+    it('should return false for arrays', () => {
+      unit
+        .bool(isEmpty([1, 2]))
+        .isFalse()
+        .bool(isEmpty(['a', {}, 6]))
+        .isFalse();
+    }); // end it
+
+    it('should return false for strings', () => {
+      unit
+        .bool(isEmpty('adsf'))
+        .isFalse()
+        .bool(isEmpty('34.6'))
+        .isFalse()
+        .bool(isEmpty('-45fg'))
+        .isFalse();
+    }); // end it
+
+    it('should return false for enumerable objects', () => {
+      unit
+        .bool(isEmpty(new String('asd')))
+        .isFalse()
+        .bool(isEmpty({
+          a: 5,
+        }))
+        .isFalse();
+    }); // end it
+  }); // end describe #isEmpty
+})(); // end IIFE
