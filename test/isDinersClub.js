02b4c2bb336a28540084905da95c2c757e2a7a5d
Steve Winberg <stevewin21@gmail.com>
2018-08-20 21:46:39 +0700

    refactor test/isDinersClub for readability

--- test/isDinersClub.js
+++ test/isDinersClub.js
@@ -1,49 +1,104 @@
-(function() {
-  var isDinersClub, noop, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const {noop} = require('underscore');
+  const isDinersClub = require('../isDinersClub');
 
-  ({noop} = require('underscore'));
+  // describe #isDinersClub
+  describe('#isDinersClub', () => {
+    it('should be a function', () => {
+      unit
+        .function(isDinersClub);
+    }); // end it
 
-  isDinersClub = require('../isDinersClub');
+    it('should return true for valid account numbers', () => {
+      unit
+        .bool(isDinersClub('30569309025904'))
+        .isTrue()
+        .bool(isDinersClub('38520000023237'))
+        .isTrue();
+    }); // end it
 
-  describe('#isDinersClub', function() {
-    it('should be a function', function() {
-      unit.function(isDinersClub);
-      return null;
-    });
-    it('should return true for valid account numbers', function() {
-      unit.bool(isDinersClub('30569309025904')).isTrue().bool(isDinersClub('38520000023237')).isTrue();
-      return null;
-    });
-    it('should return false for invalid account numbers', function() {
-      unit.bool(isDinersClub('378282246310004')).isFalse().bool(isDinersClub('378282246310006')).isFalse().bool(isDinersClub('371449635398430')).isFalse().bool(isDinersClub('371449635398432')).isFalse().bool(isDinersClub('5610591081018250')).isFalse().bool(isDinersClub('30569309025903')).isFalse().bool(isDinersClub('6011111111111117')).isFalse();
-      return null;
-    });
-    it('should return false for integers', function() {
-      unit.bool(isDinersClub(14)).isFalse().bool(isDinersClub(234987)).isFalse().bool(isDinersClub(-2398)).isFalse().bool(isDinersClub(2)).isFalse();
-      return null;
-    });
-    it('should return false for floats', function() {
-      unit.bool(isDinersClub(98.00007)).isFalse().bool(isDinersClub(-32407.3)).isFalse().bool(isDinersClub(0.1)).isFalse();
-      return null;
-    });
-    it('should return false for functions', function() {
-      unit.bool(isDinersClub(noop)).isFalse().bool(isDinersClub(isDinersClub)).isFalse();
-      return null;
-    });
-    it('should return false for invalid strings', function() {
-      unit.bool(isDinersClub('adsf')).isFalse().bool(isDinersClub('34.t')).isFalse().bool(isDinersClub('-45fg')).isFalse();
-      return null;
-    });
-    it('should return false for regexs', function() {
-      unit.bool(isDinersClub(/asd/)).isFalse().bool(isDinersClub(/\d+/)).isFalse().bool(isDinersClub(/1/)).isFalse().bool(isDinersClub(new RegExp('3'))).isFalse();
-      return null;
-    });
-    return it('should return false for arrays', function() {
-      unit.bool(isDinersClub([])).isFalse().bool(isDinersClub([1, 2, 3])).isFalse().bool(isDinersClub(['a', 5, {}])).isFalse();
-      return null;
-    });
-  });
+    it('should return false for invalid account numbers', () => {
+      unit
+        .bool(isDinersClub('378282246310004'))
+        .isFalse()
+        .bool(isDinersClub('378282246310006'))
+        .isFalse()
+        .bool(isDinersClub('371449635398430'))
+        .isFalse()
+        .bool(isDinersClub('371449635398432'))
+        .isFalse()
+        .bool(isDinersClub('5610591081018250'))
+        .isFalse()
+        .bool(isDinersClub('30569309025903'))
+        .isFalse()
+        .bool(isDinersClub('6011111111111117'))
+        .isFalse();
+    }); // end it
 
-}).call(this);
+    it('should return false for integers', () => {
+      unit
+        .bool(isDinersClub(14))
+        .isFalse()
+        .bool(isDinersClub(234987))
+        .isFalse()
+        .bool(isDinersClub(-2398))
+        .isFalse()
+        .bool(isDinersClub(2))
+        .isFalse();
+    }); // end it
+
+    it('should return false for floats', () => {
+      unit
+        .bool(isDinersClub(98.00007))
+        .isFalse()
+        .bool(isDinersClub(-32407.3))
+        .isFalse()
+        .bool(isDinersClub(0.1))
+        .isFalse();
+    }); // end it
+
+    it('should return false for functions', () => {
+      unit
+        .bool(isDinersClub(noop))
+        .isFalse()
+        .bool(isDinersClub(isDinersClub))
+        .isFalse();
+    }); // end it
+
+    it('should return false for invalid strings', () => {
+      unit
+        .bool(isDinersClub('adsf'))
+        .isFalse()
+        .bool(isDinersClub('34.t'))
+        .isFalse()
+        .bool(isDinersClub('-45fg'))
+        .isFalse();
+    }); // end it
+
+    it('should return false for regexs', () => {
+      unit
+        .bool(isDinersClub(/asd/))
+        .isFalse()
+        .bool(isDinersClub(/\d+/))
+        .isFalse()
+        .bool(isDinersClub(/1/))
+        .isFalse()
+        .bool(isDinersClub(new RegExp('3')))
+        .isFalse();
+    }); // end it
+
+    it('should return false for arrays', () => {
+      unit
+        .bool(isDinersClub([]))
+        .isFalse()
+        .bool(isDinersClub([1, 2, 3]))
+        .isFalse()
+        .bool(isDinersClub(['a', 5, {}]))
+        .isFalse();
+    }); // end it
+  }); // end describe #isDinersClub
+})(); // end IIFE
