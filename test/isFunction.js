21094fa7dcbb8d34f2e4b84730fe4150d1ddb407
Steve Winberg <stevewin21@gmail.com>
2018-08-20 22:35:50 +0700

    refactor test/isFunction for readability

--- test/isFunction.js
+++ test/isFunction.js
@@ -1,47 +1,92 @@
-(function() {
-  var isFunction, noop, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const {noop} = require('underscore');
+  const isFunction = require('../isFunction');
 
-  ({noop} = require('underscore'));
+  // describe #isFunction
+  describe('#isFunction', () => {
+    it('should be a function', () => {
+      unit
+        .function(isFunction);
+    }); // end it
 
-  isFunction = require('../isFunction');
+    it('should return true for functions', () => {
+      unit
+        .bool(isFunction(isFunction))
+        .isTrue()
+        .bool(isFunction(noop))
+        .isTrue();
+    }); // end it
 
-  describe('#isFunction', function() {
-    it('should be a function', function() {
-      unit.function(isFunction);
-      return null;
-    });
-    it('should return true for functions', function() {
-      unit.bool(isFunction(isFunction)).isTrue().bool(isFunction(noop)).isTrue();
-      return null;
-    });
-    it('should return false for integers', function() {
-      unit.bool(isFunction(1)).isFalse().bool(isFunction(234987)).isFalse().bool(isFunction(-239874)).isFalse().bool(isFunction(0)).isFalse();
-      return null;
-    });
-    it('should return false for floats', function() {
-      unit.bool(isFunction(1.1)).isFalse().bool(isFunction(-0.4)).isFalse().bool(isFunction(234.4)).isFalse().bool(isFunction(54.00000000001)).isFalse();
-      return null;
-    });
-    it('should return false for strings', function() {
-      unit.bool(isFunction('adsf')).isFalse().bool(isFunction('34.6')).isFalse().bool(isFunction('-45fg')).isFalse();
-      return null;
-    });
-    it('should return false for regexs', function() {
-      unit.bool(isFunction(/asd/)).isFalse().bool(isFunction(/\d+/)).isFalse().bool(isFunction(/1/)).isFalse().bool(isFunction(new RegExp('3'))).isFalse();
-      return null;
-    });
-    it('should return false for objects', function() {
-      unit.bool(isFunction({})).isFalse().bool(isFunction(new String('asd'))).isFalse().bool(isFunction({
-        a: 5
-      })).isFalse();
-      return null;
-    });
-    return it('should return false for arrays', function() {
-      unit.bool(isFunction([])).isFalse().bool(isFunction([1, 2])).isFalse().bool(isFunction(['a', {}, 6])).isFalse();
-      return null;
-    });
-  });
+    it('should return false for integers', () => {
+      unit
+        .bool(isFunction(1))
+        .isFalse()
+        .bool(isFunction(234987))
+        .isFalse()
+        .bool(isFunction(-239874))
+        .isFalse()
+        .bool(isFunction(0))
+        .isFalse();
+    }); // end it
 
-}).call(this);
+    it('should return false for floats', () => {
+      unit
+        .bool(isFunction(1.1))
+        .isFalse()
+        .bool(isFunction(-0.4))
+        .isFalse()
+        .bool(isFunction(234.4))
+        .isFalse()
+        .bool(isFunction(54.00000000001))
+        .isFalse();
+    }); // end it
+
+    it('should return false for strings', () => {
+      unit
+        .bool(isFunction('adsf'))
+        .isFalse()
+        .bool(isFunction('34.6'))
+        .isFalse()
+        .bool(isFunction('-45fg'))
+        .isFalse();
+    }); // end it
+
+    it('should return false for regexs', () => {
+      unit
+        .bool(isFunction(/asd/))
+        .isFalse()
+        .bool(isFunction(/\d+/))
+        .isFalse()
+        .bool(isFunction(/1/))
+        .isFalse()
+        .bool(isFunction(new RegExp('3')))
+        .isFalse();
+    }); // end it
+
+    it('should return false for objects', () => {
+      unit
+        .bool(isFunction({}))
+        .isFalse()
+        .bool(isFunction(new String('asd')))
+        .isFalse()
+        .bool(isFunction({
+          a: 5,
+        }))
+        .isFalse();
+    }); // end it
+
+    it('should return false for arrays', () => {
+      unit
+        .bool(isFunction([]))
+        .isFalse()
+        .bool(isFunction([1, 2]))
+        .isFalse()
+        .bool(isFunction(['a', {}, 6]))
+        .isFalse();
+    }); // end it
+  }); // end describe #isFunction
+})(); // end IIFE
