c3cf0c56c362d17baf1e4ce7c64a848b67f0f1a3
Steve Winberg <stevewin21@gmail.com>
2018-08-23 23:11:27 +0700

    refactor test/isString for readability

--- test/isString.js
+++ test/isString.js
@@ -1,41 +1,83 @@
-(function() {
-  var isString, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const isString = require('../isString');
 
-  isString = require('../isString');
+  // describe #isString
+  describe('#isString', () => {
+    it('should be a function', () => {
+      unit
+        .function(isString);
+    }); // end it
 
-  describe('#isString', function() {
-    it('should be a function', function() {
-      unit.function(isString);
-      return null;
-    });
-    it('should return true for strings', function() {
-      unit.bool(isString('hi')).isTrue().bool(isString('34')).isTrue().bool(isString(new String('new'))).isTrue();
-      return null;
-    });
-    it('should return false for integers', function() {
-      unit.bool(isString(1)).isFalse().bool(isString(234987)).isFalse().bool(isString(-239874)).isFalse().bool(isString(0)).isFalse();
-      return null;
-    });
-    it('should return false for floats', function() {
-      unit.bool(isString(1.1)).isFalse().bool(isString(-0.4)).isFalse().bool(isString(234.4)).isFalse().bool(isString(54.00000000001)).isFalse();
-      return null;
-    });
-    it('should return false for regexs', function() {
-      unit.bool(isString(/asd/)).isFalse().bool(isString(/\d+/)).isFalse().bool(isString(/1/)).isFalse().bool(isString(new RegExp('3'))).isFalse();
-      return null;
-    });
-    it('should return false for objects', function() {
-      unit.bool(isString({})).isFalse().bool(isString(new Error('boo'))).isFalse().bool(isString({
-        a: 5
-      })).isFalse();
-      return null;
-    });
-    return it('should return false for arrays', function() {
-      unit.bool(isString([])).isFalse().bool(isString([1, 2])).isFalse().bool(isString(['a', {}, 6])).isFalse();
-      return null;
-    });
-  });
+    it('should return true for strings', () => {
+      unit
+        .bool(isString('hi'))
+        .isTrue()
+        .bool(isString('34'))
+        .isTrue()
+        .bool(isString(new String('new')))
+        .isTrue();
+    }); // end it
 
-}).call(this);
+    it('should return false for integers', () => {
+      unit
+        .bool(isString(1))
+        .isFalse()
+        .bool(isString(234987))
+        .isFalse()
+        .bool(isString(-239874))
+        .isFalse()
+        .bool(isString(0))
+        .isFalse();
+    }); // end it
+
+    it('should return false for floats', () => {
+      unit
+        .bool(isString(1.1))
+        .isFalse()
+        .bool(isString(-0.4))
+        .isFalse()
+        .bool(isString(234.4))
+        .isFalse()
+        .bool(isString(54.00000000001))
+        .isFalse();
+    }); // end it
+
+    it('should return false for regexs', () => {
+      unit
+        .bool(isString(/asd/))
+        .isFalse()
+        .bool(isString(/\d+/))
+        .isFalse()
+        .bool(isString(/1/))
+        .isFalse()
+        .bool(isString(new RegExp('3')))
+        .isFalse();
+    }); // end it
+
+    it('should return false for objects', () => {
+      unit
+        .bool(isString({}))
+        .isFalse()
+        .bool(isString(new Error('boo')))
+        .isFalse()
+        .bool(isString({
+          a: 5,
+        }))
+        .isFalse();
+    }); // end it
+
+    it('should return false for arrays', () => {
+      unit
+        .bool(isString([]))
+        .isFalse()
+        .bool(isString([1, 2]))
+        .isFalse()
+        .bool(isString(['a', {}, 6]))
+        .isFalse();
+    }); // end it
+  }); // end describe #isString
+})(); // end IIFE
