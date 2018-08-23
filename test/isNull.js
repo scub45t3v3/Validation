2abb7629210959c09bf26d6b8432c96ca50ede7f
Steve Winberg <stevewin21@gmail.com>
2018-08-23 21:44:27 +0700

    refactor test/isNull for readability

--- test/isNull.js
+++ test/isNull.js
@@ -1,49 +1,95 @@
-(function() {
-  var isNull, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const isNull = require('../isNull');
 
-  isNull = require('../isNull');
+  // describe #isNull
+  describe('#isNull', () => {
+    it('should be a function', () => {
+      unit
+        .function(isNull);
+    }); // end it
 
-  describe('#isNull', function() {
-    it('should be a function', function() {
-      unit.function(isNull);
-      return null;
-    });
-    it('should return true for null', function() {
-      unit.bool(isNull(null)).isTrue();
-      return null;
-    });
-    it('should return false for undefined', function() {
-      unit.bool(isNull(void 0)).isFalse();
-      return null;
-    });
-    it('should return false for integers', function() {
-      unit.bool(isNull(1)).isFalse().bool(isNull(234987)).isFalse().bool(isNull(-239874)).isFalse().bool(isNull(0)).isFalse();
-      return null;
-    });
-    it('should return false for floats', function() {
-      unit.bool(isNull(1.1)).isFalse().bool(isNull(-0.4)).isFalse().bool(isNull(234.4)).isFalse().bool(isNull(54.00000000001)).isFalse();
-      return null;
-    });
-    it('should return false for strings', function() {
-      unit.bool(isNull('adsf')).isFalse().bool(isNull('34.6')).isFalse().bool(isNull('-45fg')).isFalse();
-      return null;
-    });
-    it('should return false for regexs', function() {
-      unit.bool(isNull(/asd/)).isFalse().bool(isNull(/\d+/)).isFalse().bool(isNull(/1/)).isFalse().bool(isNull(new RegExp('3'))).isFalse();
-      return null;
-    });
-    it('should return false for objects', function() {
-      unit.bool(isNull({})).isFalse().bool(isNull(new String('asd'))).isFalse().bool(isNull({
-        a: 5
-      })).isFalse();
-      return null;
-    });
-    return it('should return false for arrays', function() {
-      unit.bool(isNull([])).isFalse().bool(isNull([1, 2])).isFalse().bool(isNull(['a', {}, 6])).isFalse();
-      return null;
-    });
-  });
+    it('should return true for null', () => {
+      unit
+        .bool(isNull(null))
+        .isTrue();
+    }); // end it
 
-}).call(this);
+    it('should return false for undefined', () => {
+      unit
+        .bool(isNull(undefined))
+        .isFalse();
+    }); // end it
+
+    it('should return false for integers', () => {
+      unit
+        .bool(isNull(1))
+        .isFalse()
+        .bool(isNull(234987))
+        .isFalse()
+        .bool(isNull(-239874))
+        .isFalse()
+        .bool(isNull(0))
+        .isFalse();
+    }); // end it
+
+    it('should return false for floats', () => {
+      unit
+        .bool(isNull(1.1))
+        .isFalse()
+        .bool(isNull(-0.4))
+        .isFalse()
+        .bool(isNull(234.4))
+        .isFalse()
+        .bool(isNull(54.00000000001))
+        .isFalse();
+    }); // end it
+
+    it('should return false for strings', () => {
+      unit
+        .bool(isNull('adsf'))
+        .isFalse()
+        .bool(isNull('34.6'))
+        .isFalse()
+        .bool(isNull('-45fg'))
+        .isFalse();
+    }); // end it
+
+    it('should return false for regexs', () => {
+      unit
+        .bool(isNull(/asd/))
+        .isFalse()
+        .bool(isNull(/\d+/))
+        .isFalse()
+        .bool(isNull(/1/))
+        .isFalse()
+        .bool(isNull(new RegExp('3')))
+        .isFalse();
+    }); // end it
+
+    it('should return false for objects', () => {
+      unit
+        .bool(isNull({}))
+        .isFalse()
+        .bool(isNull(new String('asd')))
+        .isFalse()
+        .bool(isNull({
+          a: 5,
+        }))
+        .isFalse();
+    }); // end it
+
+    it('should return false for arrays', () => {
+      unit
+        .bool(isNull([]))
+        .isFalse()
+        .bool(isNull([1, 2]))
+        .isFalse()
+        .bool(isNull(['a', {}, 6]))
+        .isFalse();
+    }); // end it
+  }); // end describe #isNull
+})(); // end IIFE
