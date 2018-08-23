82d83b2ff7a5553eff84d51fb8f3eaabd99e9834
Steve Winberg <stevewin21@gmail.com>
2018-08-23 22:51:01 +0700

    refactor test/isSealed for readability

--- test/isSealed.js
+++ test/isSealed.js
@@ -1,72 +1,143 @@
-(function() {
-  var isSealed, noop, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const {noop} = require('underscore');
+  const isSealed = require('../isSealed');
 
-  ({noop} = require('underscore'));
+  // describe #isSealed
+  describe('#isSealed', () => {
+    it('should be a function', () => {
+      unit
+        .function(isSealed);
+    }); // end it
 
-  isSealed = require('../isSealed');
+    it('should return true for sealed objects', () => {
+      unit
+        .bool(isSealed(Object.seal({
+          a: 5,
+        })))
+        .isTrue()
+        .bool(isSealed(Object.seal(new String('hi'))))
+        .isTrue();
+    }); // end it
 
-  describe('#isSealed', function() {
-    it('should be a function', function() {
-      unit.function(isSealed);
-      return null;
-    });
-    it('should return true for sealed objects', function() {
-      unit.bool(isSealed(Object.seal({
-        a: 5
-      }))).isTrue().bool(isSealed(Object.seal(new String('hi')))).isTrue();
-      return null;
-    });
-    it('should return true for frozen objects', function() {
-      unit.bool(isSealed(Object.freeze({
-        a: 5
-      }))).isTrue().bool(isSealed(Object.freeze(new String('hi')))).isTrue();
-      return null;
-    });
-    it('should return true for empty objects with preventExtensions', function() {
-      unit.bool(isSealed(Object.preventExtensions({}))).isTrue().bool(isSealed(Object.preventExtensions(new String('hi')))).isTrue().bool(isSealed(Object.preventExtensions(new Number(5)))).isTrue();
-      return null;
-    });
-    it('should return true for primitives', function() {
-      unit.bool(isSealed(1)).isTrue().bool(isSealed(234987)).isTrue().bool(isSealed(-239874)).isTrue().bool(isSealed(0)).isTrue().bool(isSealed(1.1)).isTrue().bool(isSealed(-0.4)).isTrue().bool(isSealed(234.4)).isTrue().bool(isSealed(54.00000000001)).isTrue().bool(isSealed('adsf')).isTrue().bool(isSealed('34.6')).isTrue().bool(isSealed('-45fg')).isTrue();
-      return null;
-    });
-    it('should return false for object literals', function() {
-      unit.bool(isSealed({})).isFalse().bool(isSealed({
-        a: 5
-      })).isFalse();
-      return null;
-    });
-    it('should return false for arrays', function() {
-      unit.bool(isSealed([])).isFalse().bool(isSealed([1, 2])).isFalse().bool(isSealed(new Array('a', {}, 6))).isFalse();
-      return null;
-    });
-    it('should return false for regexs', function() {
-      unit.bool(isSealed(/asd/)).isFalse().bool(isSealed(/\d+/)).isFalse().bool(isSealed(/1/)).isFalse().bool(isSealed(new RegExp('3'))).isFalse();
-      return null;
-    });
-    it('should return false for functions', function() {
-      unit.bool(isSealed(noop)).isFalse().bool(isSealed(isSealed)).isFalse();
-      return null;
-    });
-    it('should return false for Map', function() {
-      unit.bool(isSealed(new Map())).isFalse().bool(isSealed(new Map([['a', 5], ['b', '$']]))).isFalse();
-      return null;
-    });
-    it('should return false for Set', function() {
-      unit.bool(isSealed(new Set())).isFalse().bool(isSealed(new Set().add(1))).isFalse().bool(isSealed(new Set([1, 2, 3]))).isFalse();
-      return null;
-    });
-    return it('should return false for non empty objects with preventExtensions', function() {
-      unit.bool(isSealed(Object.preventExtensions({
-        a: 5
-      }))).isFalse().bool(isSealed(Object.preventExtensions({
-        a: 'hi',
-        b: true
-      }))).isFalse();
-      return null;
-    });
-  });
+    it('should return true for frozen objects', () => {
+      unit
+        .bool(isSealed(Object.freeze({
+          a: 5,
+        })))
+        .isTrue()
+        .bool(isSealed(Object.freeze(new String('hi'))))
+        .isTrue();
+    }); // end it
 
-}).call(this);
+    it('should return true for empty objects with preventExtensions', () => {
+      unit
+        .bool(isSealed(Object.preventExtensions({})))
+        .isTrue()
+        .bool(isSealed(Object.preventExtensions(new String('hi'))))
+        .isTrue()
+        .bool(isSealed(Object.preventExtensions(new Number(5))))
+        .isTrue();
+    }); // end it
+
+    it('should return true for primitives', () => {
+      unit
+        .bool(isSealed(1))
+        .isTrue()
+        .bool(isSealed(234987))
+        .isTrue()
+        .bool(isSealed(-239874))
+        .isTrue()
+        .bool(isSealed(0))
+        .isTrue()
+        .bool(isSealed(1.1))
+        .isTrue()
+        .bool(isSealed(-0.4))
+        .isTrue()
+        .bool(isSealed(234.4))
+        .isTrue()
+        .bool(isSealed(54.00000000001))
+        .isTrue()
+        .bool(isSealed('adsf'))
+        .isTrue()
+        .bool(isSealed('34.6'))
+        .isTrue()
+        .bool(isSealed('-45fg'))
+        .isTrue();
+    }); // end it
+
+    it('should return false for object literals', () => {
+      unit
+        .bool(isSealed({}))
+        .isFalse()
+        .bool(isSealed({
+          a: 5,
+        }))
+        .isFalse();
+    }); // end it
+
+    it('should return false for arrays', () => {
+      unit
+        .bool(isSealed([]))
+        .isFalse()
+        .bool(isSealed([1, 2]))
+        .isFalse()
+        .bool(isSealed(new Array('a', {}, 6)))
+        .isFalse();
+    }); // end it
+
+    it('should return false for regexs', () => {
+      unit
+        .bool(isSealed(/asd/))
+        .isFalse()
+        .bool(isSealed(/\d+/))
+        .isFalse()
+        .bool(isSealed(/1/))
+        .isFalse()
+        .bool(isSealed(new RegExp('3')))
+        .isFalse();
+    }); // end it
+
+    it('should return false for functions', () => {
+      unit
+        .bool(isSealed(noop))
+        .isFalse()
+        .bool(isSealed(isSealed))
+        .isFalse();
+    }); // end it
+
+    it('should return false for Map', () => {
+      unit
+        .bool(isSealed(new Map()))
+        .isFalse()
+        .bool(isSealed(new Map([['a', 5], ['b', '$']])))
+        .isFalse();
+    }); // end it
+
+    it('should return false for Set', () => {
+      unit
+        .bool(isSealed(new Set()))
+        .isFalse()
+        .bool(isSealed(new Set().add(1)))
+        .isFalse()
+        .bool(isSealed(new Set([1, 2, 3])))
+        .isFalse();
+    }); // end it
+
+    it('should return false for non empty objects with preventExtensions', () => {
+      unit
+        .bool(isSealed(Object.preventExtensions({
+          a: 5,
+        })))
+        .isFalse()
+        .bool(isSealed(Object.preventExtensions({
+          a: 'hi',
+          b: true,
+        })))
+        .isFalse();
+    }); // end it
+  }); // end describe #isSealed
+})(); // end IIFE
