85509786dde7167a9976c6782cee737907873f9f
Steve Winberg <stevewin21@gmail.com>
2018-08-23 21:31:03 +0700

    refactor test/isMatch for readability

--- test/isMatch.js
+++ test/isMatch.js
@@ -1,49 +1,65 @@
-(function() {
-  var isMatch, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const isMatch = require('../isMatch');
 
-  isMatch = require('../isMatch');
+  // describe #isMatch
+  describe('#isMatch', () => {
+    it('should be a function', () => {
+      unit
+        .function(isMatch);
+    }); // end it
 
-  describe('#isMatch', function() {
-    it('should be a function', function() {
-      unit.function(isMatch);
-      return null;
-    });
-    it('should return true for objects containing matching properties', function() {
-      var test;
-      test = {
+    it('should return true for objects containing matching properties', () => {
+      const test = {
         a: 5,
-        b: 'bye'
+        b: 'bye',
       };
-      unit.bool(isMatch(test, {
-        a: 5
-      })).isTrue().bool(isMatch(test, {
-        b: 'bye'
-      })).isTrue().bool(isMatch(test, {
-        a: 5,
-        b: 'bye'
-      })).isTrue().bool(isMatch(test, {})).isTrue();
-      return null;
-    });
-    return it('should return false for objects not matching properties', function() {
-      var test;
-      test = {
-        a: 5,
-        b: 'bye'
-      };
-      unit.bool(isMatch(test, {
-        a: 6
-      })).isFalse().bool(isMatch(test, {
-        b: 'hi'
-      })).isFalse().bool(isMatch(test, {
-        a: 5,
-        b: 'b'
-      })).isFalse().bool(isMatch(test, {
-        c: 5
-      })).isFalse();
-      return null;
-    });
-  });
 
-}).call(this);
+      unit
+        .bool(isMatch(test, {
+          a: 5,
+        }))
+        .isTrue()
+        .bool(isMatch(test, {
+          b: 'bye',
+        }))
+        .isTrue()
+        .bool(isMatch(test, {
+          a: 5,
+          b: 'bye',
+        }))
+        .isTrue()
+        .bool(isMatch(test, {}))
+        .isTrue();
+    }); // end it
+
+    it('should return false for objects not matching properties', () => {
+      const test = {
+        a: 5,
+        b: 'bye',
+      };
+
+      unit
+        .bool(isMatch(test, {
+          a: 6,
+        }))
+        .isFalse()
+        .bool(isMatch(test, {
+          b: 'hi',
+        }))
+        .isFalse()
+        .bool(isMatch(test, {
+          a: 5,
+          b: 'b',
+        }))
+        .isFalse()
+        .bool(isMatch(test, {
+          c: 5,
+        }))
+        .isFalse();
+    }); // end it
+  }); // end describe #isMatch
+})(); // end IIFE
