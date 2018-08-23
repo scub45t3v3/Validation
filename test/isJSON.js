09062c38edcb44aa71127eef90e0b3960b073a62
Steve Winberg <stevewin21@gmail.com>
2018-08-23 20:46:50 +0700

    refactor test/isJSON for readability

--- test/isJSON.js
+++ test/isJSON.js
@@ -1,45 +1,96 @@
-(function() {
-  var isJSON, noop, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const {noop} = require('underscore');
+  const isJSON = require('../isJSON');
 
-  ({noop} = require('underscore'));
+  // describe #isJSON
+  describe('#isJSON', () => {
+    it('should be a function', () => {
+      unit
+        .function(isJSON);
+    }); // end it
 
-  isJSON = require('../isJSON');
+    it('should return true for valid JSON strings', () => {
+      unit
+        .bool(isJSON('{}'))
+        .isTrue()
+        .bool(isJSON('{"a":[1,2,3],"b":6}'))
+        .isTrue()
+        .bool(isJSON('[1,"2",6]'))
+        .isTrue()
+        .bool(isJSON('{"a":{"z":true,"x":5.6,"y":"h"},"b":-6}'))
+        .isTrue()
+        .bool(isJSON('{"a":false,"b":null}'))
+        .isTrue();
+    }); // end it
 
-  describe('#isJSON', function() {
-    it('should be a function', function() {
-      unit.function(isJSON);
-      return null;
-    });
-    it('should return true for valid JSON strings', function() {
-      unit.bool(isJSON('{}')).isTrue().bool(isJSON('{"a":[1,2,3],"b":6}')).isTrue().bool(isJSON('[1,"2",6]')).isTrue().bool(isJSON('{"a":{"z":true,"x":5.6,"y":"h"},"b":-6}')).isTrue().bool(isJSON('{"a":false,"b":null}')).isTrue();
-      return null;
-    });
-    it('should return true for integers', function() {
-      unit.bool(isJSON(14)).isTrue().bool(isJSON(234987)).isTrue().bool(isJSON(-2398)).isTrue().bool(isJSON(2)).isTrue();
-      return null;
-    });
-    it('should return true for floats', function() {
-      unit.bool(isJSON(98.00007)).isTrue().bool(isJSON(-32407.3)).isTrue().bool(isJSON(0.1)).isTrue();
-      return null;
-    });
-    it('should return false for invalid JSON strings', function() {
-      unit.bool(isJSON('we48tuer')).isFalse().bool(isJSON('[a,6,h]')).isFalse().bool(isJSON('{a:false}')).isFalse().bool(isJSON('{"a":{6,"b":true}')).isFalse().bool(isJSON('{"b";false}')).isFalse();
-      return null;
-    });
-    it('should return false for functions', function() {
-      unit.bool(isJSON(noop)).isFalse().bool(isJSON(isJSON)).isFalse();
-      return null;
-    });
-    it('should return false for regexs', function() {
-      unit.bool(isJSON(/asd/)).isFalse().bool(isJSON(/\d+/)).isFalse().bool(isJSON(/1/)).isFalse().bool(isJSON(new RegExp('3'))).isFalse();
-      return null;
-    });
-    return it('should return false for arrays', function() {
-      unit.bool(isJSON([])).isFalse().bool(isJSON([1, 2, 3])).isFalse().bool(isJSON(['a', 5, {}])).isFalse();
-      return null;
-    });
-  });
+    it('should return true for integers', () => {
+      unit
+        .bool(isJSON(14))
+        .isTrue()
+        .bool(isJSON(234987))
+        .isTrue()
+        .bool(isJSON(-2398))
+        .isTrue()
+        .bool(isJSON(2))
+        .isTrue();
+    }); // end it
 
-}).call(this);
+    it('should return true for floats', () => {
+      unit
+        .bool(isJSON(98.00007))
+        .isTrue()
+        .bool(isJSON(-32407.3))
+        .isTrue()
+        .bool(isJSON(0.1))
+        .isTrue();
+    }); // end it
+
+    it('should return false for invalid JSON strings', () => {
+      unit
+        .bool(isJSON('we48tuer'))
+        .isFalse()
+        .bool(isJSON('[a,6,h]'))
+        .isFalse()
+        .bool(isJSON('{a:false}'))
+        .isFalse()
+        .bool(isJSON('{"a":{6,"b":true}'))
+        .isFalse()
+        .bool(isJSON('{"b";false}'))
+        .isFalse();
+    }); // end it
+
+    it('should return false for functions', () => {
+      unit
+        .bool(isJSON(noop))
+        .isFalse()
+        .bool(isJSON(isJSON))
+        .isFalse();
+    }); // end it
+
+    it('should return false for regexs', () => {
+      unit
+        .bool(isJSON(/asd/))
+        .isFalse()
+        .bool(isJSON(/\d+/))
+        .isFalse()
+        .bool(isJSON(/1/))
+        .isFalse()
+        .bool(isJSON(new RegExp('3')))
+        .isFalse();
+    }); // end it
+
+    it('should return false for arrays', () => {
+      unit
+        .bool(isJSON([]))
+        .isFalse()
+        .bool(isJSON([1, 2, 3]))
+        .isFalse()
+        .bool(isJSON(['a', 5, {}]))
+        .isFalse();
+    }); // end it
+  }); // end describe #isJSON
+})(); // end IIFE
