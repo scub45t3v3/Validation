46c20f02aed76dc266411e9c7fc4cb20ade66b54
Steve Winberg <stevewin21@gmail.com>
2018-08-23 22:25:33 +0700

    refactor test/isRegExp for readability

--- test/isRegExp.js
+++ test/isRegExp.js
@@ -1,41 +1,83 @@
-(function() {
-  var isRegExp, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const isRegExp = require('../isRegExp');
 
-  isRegExp = require('../isRegExp');
+  // describe #isRegExp
+  describe('#isRegExp', () => {
+    it('should be a function', () => {
+      unit
+        .function(isRegExp);
+    }); // end it
 
-  describe('#isRegExp', function() {
-    it('should be a function', function() {
-      unit.function(isRegExp);
-      return null;
-    });
-    it('should return true for regexs', function() {
-      unit.bool(isRegExp(/asd/)).isTrue().bool(isRegExp(/\d+/)).isTrue().bool(isRegExp(/1/)).isTrue().bool(isRegExp(new RegExp('3'))).isTrue();
-      return null;
-    });
-    it('should return false for integers', function() {
-      unit.bool(isRegExp(1)).isFalse().bool(isRegExp(234987)).isFalse().bool(isRegExp(-239874)).isFalse().bool(isRegExp(0)).isFalse();
-      return null;
-    });
-    it('should return false for floats', function() {
-      unit.bool(isRegExp(1.1)).isFalse().bool(isRegExp(-0.4)).isFalse().bool(isRegExp(234.4)).isFalse().bool(isRegExp(54.00000000001)).isFalse();
-      return null;
-    });
-    it('should return false for strings', function() {
-      unit.bool(isRegExp('adsf')).isFalse().bool(isRegExp('34.6')).isFalse().bool(isRegExp('-45fg')).isFalse();
-      return null;
-    });
-    it('should return false for objects', function() {
-      unit.bool(isRegExp({})).isFalse().bool(isRegExp(new String('asd'))).isFalse().bool(isRegExp({
-        a: 5
-      })).isFalse();
-      return null;
-    });
-    return it('should return false for arrays', function() {
-      unit.bool(isRegExp([])).isFalse().bool(isRegExp([1, 2])).isFalse().bool(isRegExp(['a', {}, 6])).isFalse();
-      return null;
-    });
-  });
+    it('should return true for regexs', () => {
+      unit
+        .bool(isRegExp(/asd/))
+        .isTrue()
+        .bool(isRegExp(/\d+/))
+        .isTrue()
+        .bool(isRegExp(/1/))
+        .isTrue()
+        .bool(isRegExp(new RegExp('3')))
+        .isTrue();
+    }); // end it
 
-}).call(this);
+    it('should return false for integers', () => {
+      unit
+        .bool(isRegExp(1))
+        .isFalse()
+        .bool(isRegExp(234987))
+        .isFalse()
+        .bool(isRegExp(-239874))
+        .isFalse()
+        .bool(isRegExp(0))
+        .isFalse();
+    }); // end it
+
+    it('should return false for floats', () => {
+      unit
+        .bool(isRegExp(1.1))
+        .isFalse()
+        .bool(isRegExp(-0.4))
+        .isFalse()
+        .bool(isRegExp(234.4))
+        .isFalse()
+        .bool(isRegExp(54.00000000001))
+        .isFalse();
+    }); // end it
+
+    it('should return false for strings', () => {
+      unit
+        .bool(isRegExp('adsf'))
+        .isFalse()
+        .bool(isRegExp('34.6'))
+        .isFalse()
+        .bool(isRegExp('-45fg'))
+        .isFalse();
+    }); // end it
+
+    it('should return false for objects', () => {
+      unit
+        .bool(isRegExp({}))
+        .isFalse()
+        .bool(isRegExp(new String('asd')))
+        .isFalse()
+        .bool(isRegExp({
+          a: 5,
+        }))
+        .isFalse();
+    }); // end it
+
+    it('should return false for arrays', () => {
+      unit
+        .bool(isRegExp([]))
+        .isFalse()
+        .bool(isRegExp([1, 2]))
+        .isFalse()
+        .bool(isRegExp(['a', {}, 6]))
+        .isFalse();
+    }); // end it
+  }); // end describe #isRegExp
+})(); // end IIFE
