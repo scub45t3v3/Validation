5ea0944f68cff19ae546c85aa5db09ca7c1e38df
Steve Winberg <stevewin21@gmail.com>
2018-08-23 21:54:38 +0700

    refactor test/isPlural for readability

--- test/isPlural.js
+++ test/isPlural.js
@@ -1,53 +1,110 @@
-(function() {
-  var isPlural, noop, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // include dependencies
+  const unit = require('unit.js');
+  const {noop} = require('underscore');
+  const isPlural = require('../isPlural');
 
-  ({noop} = require('underscore'));
+  // describe #isPlural
+  describe('#isPlural', () => {
+    it('should be a function', () => {
+      unit
+        .function(isPlural);
+    }); // end it
 
-  isPlural = require('../isPlural');
+    it('should return true for plural strings', () => {
+      unit
+        .bool(isPlural('items'))
+        .isTrue()
+        .bool(isPlural('cars'))
+        .isTrue()
+        .bool(isPlural('bars'))
+        .isTrue()
+        .bool(isPlural('tables'))
+        .isTrue()
+        .bool(isPlural('chairs'))
+        .isTrue();
+    }); // end it
 
-  describe('#isPlural', function() {
-    it('should be a function', function() {
-      unit.function(isPlural);
-      return null;
-    });
-    it('should return true for plural strings', function() {
-      unit.bool(isPlural('items')).isTrue().bool(isPlural('cars')).isTrue().bool(isPlural('bars')).isTrue().bool(isPlural('tables')).isTrue().bool(isPlural('chairs')).isTrue();
-      return null;
-    });
-    it('should return false for singular strings', function() {
-      unit.bool(isPlural('item')).isFalse().bool(isPlural('car')).isFalse().bool(isPlural('bar')).isFalse().bool(isPlural('table')).isFalse().bool(isPlural('chair')).isFalse();
-      return null;
-    });
-    it('should return false for integers', function() {
-      unit.bool(isPlural(14)).isFalse().bool(isPlural(234987)).isFalse().bool(isPlural(-2398)).isFalse().bool(isPlural(2)).isFalse();
-      return null;
-    });
-    it('should return false for floats', function() {
-      unit.bool(isPlural(98.00007)).isFalse().bool(isPlural(-32407.3)).isFalse().bool(isPlural(0.1)).isFalse();
-      return null;
-    });
-    it('should return false for functions', function() {
-      unit.bool(isPlural(noop)).isFalse().bool(isPlural(isPlural)).isFalse();
-      return null;
-    });
-    it('should return false for regexs', function() {
-      unit.bool(isPlural(/asd/)).isFalse().bool(isPlural(/\d+/)).isFalse().bool(isPlural(/1/)).isFalse().bool(isPlural(new RegExp('3'))).isFalse();
-      return null;
-    });
-    it('should return false for arrays', function() {
-      unit.bool(isPlural([])).isFalse().bool(isPlural([1, 2, 3])).isFalse().bool(isPlural(['a', 5, {}])).isFalse();
-      return null;
-    });
-    it('should return false for null', function() {
-      unit.bool(isPlural(null)).isFalse();
-      return null;
-    });
-    return it('should return false for undefined', function() {
-      unit.bool(isPlural()).isFalse().bool(isPlural(void 0)).isFalse();
-      return null;
-    });
-  });
+    it('should return false for singular strings', () => {
+      unit
+        .bool(isPlural('item'))
+        .isFalse()
+        .bool(isPlural('car'))
+        .isFalse()
+        .bool(isPlural('bar'))
+        .isFalse()
+        .bool(isPlural('table'))
+        .isFalse()
+        .bool(isPlural('chair'))
+        .isFalse();
+    }); // end it
 
-}).call(this);
+    it('should return false for integers', () => {
+      unit
+        .bool(isPlural(14))
+        .isFalse()
+        .bool(isPlural(234987))
+        .isFalse()
+        .bool(isPlural(-2398))
+        .isFalse()
+        .bool(isPlural(2))
+        .isFalse();
+    }); // end it
+
+    it('should return false for floats', () => {
+      unit
+        .bool(isPlural(98.00007))
+        .isFalse()
+        .bool(isPlural(-32407.3))
+        .isFalse()
+        .bool(isPlural(0.1))
+        .isFalse();
+    }); // end it
+
+    it('should return false for functions', () => {
+      unit
+        .bool(isPlural(noop))
+        .isFalse()
+        .bool(isPlural(isPlural))
+        .isFalse();
+    }); // end it
+
+    it('should return false for regexs', () => {
+      unit
+        .bool(isPlural(/asd/))
+        .isFalse()
+        .bool(isPlural(/\d+/))
+        .isFalse()
+        .bool(isPlural(/1/))
+        .isFalse()
+        .bool(isPlural(new RegExp('3')))
+        .isFalse();
+    }); // end it
+
+    it('should return false for arrays', () => {
+      unit
+        .bool(isPlural([]))
+        .isFalse()
+        .bool(isPlural([1, 2, 3]))
+        .isFalse()
+        .bool(isPlural(['a', 5, {}]))
+        .isFalse();
+    }); // end it
+
+    it('should return false for null', () => {
+      unit
+        .bool(isPlural(null))
+        .isFalse();
+    }); // end it
+
+    it('should return false for undefined', () => {
+      unit
+        .bool(isPlural())
+        .isFalse()
+        .bool(isPlural(undefined))
+        .isFalse();
+    }); // end it
+  }); // end describe #isPlural
+})(); // end IIFE
