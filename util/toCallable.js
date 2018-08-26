c406f7b7d1cf5e6775d25a0e57a0034161167e8d
Steve Winberg <stevewin21@gmail.com>
2018-08-26 12:10:42 +0700

    refactor util/toCallable for readability

--- util/toCallable.js
+++ util/toCallable.js
@@ -1,38 +1,41 @@
-(function() {
-  var _, debug, isArray, isFunction, isRegExp, isString, toCallable;
+'use strict';
 
-  _ = require('underscore');
+(() => {
+  // include dependencies
+  const _ = require('underscore');
+  const debug = require('debug')('@scuba-squad:validation:util:toCallable');
+  const isFunction = require('../isFunction');
+  const isString = require('../isString');
+  const isRegExp = require('../isRegExp');
+  const isArray = require('../isArray');
 
-  debug = require('debug')('@scuba-squad:validation:util:toCallable');
-
-  isFunction = require('../isFunction');
-
-  isString = require('../isString');
-
-  isRegExp = require('../isRegExp');
-
-  isArray = require('../isArray');
-
-  toCallable = function(value) {
-    var func;
+  const toCallable = function(value) {
     debug('call:toCallable(%o)', value);
+
     if (isFunction(value)) {
       return value;
     }
+
     if (isString(value)) {
       return require(`../${value}`);
     }
+
     if (isRegExp(value)) {
       return value.test.bind(value);
     }
+
     if (isArray(value)) {
-      func = toCallable(_.first(value));
-      return _.partial(func, _, ..._.rest(value, 1));
+      const func = toCallable(_.first(value));
+      const rest = _.rest(value, 1);
+
+      return _.partial(func, _, ...rest);
     }
+
     debug('error:value %o can not be converted to a function', value);
+
     throw new TypeError(`${value} can not be called`);
-  };
+  }; // end toCallable
 
+  // export as commmonjs module
   module.exports = toCallable;
-
-}).call(this);
+})(); // end IIFE
