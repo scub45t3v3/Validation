9f612b68c4ee2f54d4133a890afcb7f6056db21b
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isPhoneNumber.js

--- isPhoneNumber.js
+++ isPhoneNumber.js
@@ -1,43 +1,41 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isPhoneNumber');
-  const {PhoneNumberUtil} = require('google-libphonenumber');
-  const Country = require('@scuba-squad/country');
-  const isString = require('./isString');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isPhoneNumber');
+const {PhoneNumberUtil} = require('google-libphonenumber');
+const Country = require('@scuba-squad/country');
+const isString = require('./isString');
 
-  const isPhoneNumber = (value, country) => {
-    debug('call:isPhoneNumber(%o, %o)', value, country);
+const isPhoneNumber = (value, country) => {
+  debug('call:isPhoneNumber(%o, %o)', value, country);
 
-    const util = PhoneNumberUtil.getInstance();
+  const util = PhoneNumberUtil.getInstance();
 
-    if (isString(country)) {
-      const iso = country.trim().toUpperCase();
+  if (isString(country)) {
+    const iso = country.trim().toUpperCase();
 
-      country = Country.getByIso2Code(iso);
-      country || (country = Country.getByIso3Code(iso));
-      country || (country = Country.getByIsoNumericCode(iso));
-    }
+    country = Country.getByIso2Code(iso);
+    country || (country = Country.getByIso3Code(iso));
+    country || (country = Country.getByIsoNumericCode(iso));
+  }
 
-    try {
-      const iso = (country && country.iso2Code) || undefined;
-      const parsed = util.parse(value, iso);
+  try {
+    const iso = (country && country.iso2Code) || undefined;
+    const parsed = util.parse(value, iso);
 
-      if (!util.isValidNumber(parsed)) {
-        return false;
-      }
-
-      if (iso && util.getRegionCodeForNumber(parsed) !== iso) {
-        return false;
-      }
-
-      return true;
-    } catch (error) {
+    if (!util.isValidNumber(parsed)) {
       return false;
     }
-  };
 
-  // export as commonjs module
-  module.exports = isPhoneNumber;
-})(); // end IIFE
+    if (iso && util.getRegionCodeForNumber(parsed) !== iso) {
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+// export as commonjs module
+module.exports = isPhoneNumber;
