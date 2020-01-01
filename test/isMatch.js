223a42a9b864158a2e15ae9d7ff8e708f74f2657
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:36:57 +0700

    remove iife from test/isMatch.js

--- test/isMatch.js
+++ test/isMatch.js
@@ -1,65 +1,63 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const isMatch = require('../isMatch');
+// include dependencies
+const unit = require('unit.js');
+const isMatch = require('../isMatch');
 
-  // describe #isMatch
-  describe('#isMatch', () => {
-    it('should be a function', () => {
-      unit
-        .function(isMatch);
-    }); // end it
+// describe #isMatch
+describe('#isMatch', () => {
+  it('should be a function', () => {
+    unit
+      .function(isMatch);
+  }); // end it
 
-    it('should return true for objects containing matching properties', () => {
-      const test = {
+  it('should return true for objects containing matching properties', () => {
+    const test = {
+      a: 5,
+      b: 'bye',
+    };
+
+    unit
+      .bool(isMatch(test, {
+        a: 5,
+      }))
+      .isTrue()
+      .bool(isMatch(test, {
+        b: 'bye',
+      }))
+      .isTrue()
+      .bool(isMatch(test, {
         a: 5,
         b: 'bye',
-      };
+      }))
+      .isTrue()
+      .bool(isMatch(test, {}))
+      .isTrue();
+  }); // end it
 
-      unit
-        .bool(isMatch(test, {
-          a: 5,
-        }))
-        .isTrue()
-        .bool(isMatch(test, {
-          b: 'bye',
-        }))
-        .isTrue()
-        .bool(isMatch(test, {
-          a: 5,
-          b: 'bye',
-        }))
-        .isTrue()
-        .bool(isMatch(test, {}))
-        .isTrue();
-    }); // end it
+  it('should return false for objects not matching properties', () => {
+    const test = {
+      a: 5,
+      b: 'bye',
+    };
 
-    it('should return false for objects not matching properties', () => {
-      const test = {
+    unit
+      .bool(isMatch(test, {
+        a: 6,
+      }))
+      .isFalse()
+      .bool(isMatch(test, {
+        b: 'hi',
+      }))
+      .isFalse()
+      .bool(isMatch(test, {
         a: 5,
-        b: 'bye',
-      };
-
-      unit
-        .bool(isMatch(test, {
-          a: 6,
-        }))
-        .isFalse()
-        .bool(isMatch(test, {
-          b: 'hi',
-        }))
-        .isFalse()
-        .bool(isMatch(test, {
-          a: 5,
-          b: 'b',
-        }))
-        .isFalse()
-        .bool(isMatch(test, {
-          c: 5,
-        }))
-        .isFalse();
-    }); // end it
-  }); // end describe #isMatch
-})(); // end IIFE
+        b: 'b',
+      }))
+      .isFalse()
+      .bool(isMatch(test, {
+        c: 5,
+      }))
+      .isFalse();
+  }); // end it
+}); // end describe #isMatch
