a14640298b6583864c00bc619415628ed8a4553d
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isURL.js

--- isURL.js
+++ isURL.js
@@ -1,24 +1,22 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isURL');
-  const isDomainName = require('./isDomainName');
-  const isIP = require('./isIP');
-  const REGEX = /^[a-z][a-z\d+.-]+:\/\/(?:(?:[\w!$&'()*+,;:=~.-]|%[\da-f]{2})+@)?([^\s:[\]/%?#@]+|\[[:.%\w]+\])(?::\d+)?(?:\/(?:[\w!$&'()*+,;:@=~.-]|%[\da-f]{2})+)*\/?(?:\?(?:[\w!$&'()*+,;:@=~?/.-]|%[\da-f]{2})*)?(?:#(?:[\w!$&'()*+,;:@=~?/.-]|%[\da-f]{2})*)?$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isURL');
+const isDomainName = require('./isDomainName');
+const isIP = require('./isIP');
+const REGEX = /^[a-z][a-z\d+.-]+:\/\/(?:(?:[\w!$&'()*+,;:=~.-]|%[\da-f]{2})+@)?([^\s:[\]/%?#@]+|\[[:.%\w]+\])(?::\d+)?(?:\/(?:[\w!$&'()*+,;:@=~.-]|%[\da-f]{2})+)*\/?(?:\?(?:[\w!$&'()*+,;:@=~?/.-]|%[\da-f]{2})*)?(?:#(?:[\w!$&'()*+,;:@=~?/.-]|%[\da-f]{2})*)?$/i;
 
-  const isURL = (value) => {
-    debug('call:isURL(%o)', value);
+const isURL = (value) => {
+  debug('call:isURL(%o)', value);
 
-    try {
-      value = value.match(REGEX)[1].replace(/^\[|\]$/g, '');
+  try {
+    value = value.match(REGEX)[1].replace(/^\[|\]$/g, '');
 
-      return isDomainName(value) || isIP(value);
-    } catch (error) {
-      return false;
-    }
-  };
+    return isDomainName(value) || isIP(value);
+  } catch (error) {
+    return false;
+  }
+};
 
-  // export as commonjs module
-  module.exports = isURL;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isURL;
