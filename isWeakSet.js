fc98f07998113ef09aab3f470fb59b5df75ac820
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isWeakSet.js

--- isWeakSet.js
+++ isWeakSet.js
@@ -1,15 +1,13 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isWeakSet');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isWeakSet');
 
-  const isWeakSet = (value) => {
-    debug('call:isWeakSet(%o)', value);
+const isWeakSet = (value) => {
+  debug('call:isWeakSet(%o)', value);
 
-    return value instanceof WeakSet;
-  };
+  return value instanceof WeakSet;
+};
 
-  // export as commonjs module
-  module.exports = isWeakSet;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isWeakSet;
