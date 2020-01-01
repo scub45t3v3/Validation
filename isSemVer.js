11023e2d70f5d3bd4d58f1a29aa16a3164cddb0d
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isSemVer.js

--- isSemVer.js
+++ isSemVer.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isSemVer');
-  const REGEX = /^(?:\d+\.){2}\d+(?:-(?:0|[1-9]\d*|\d*[a-z-][a-z\d-]*)(?:\.(?:0|[1-9]\d*|\d*[a-z-][a-z\d-]*))*)?(?:\+[a-z\d-]+(?:\.[a-z\d-]+)*)?$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isSemVer');
+const REGEX = /^(?:\d+\.){2}\d+(?:-(?:0|[1-9]\d*|\d*[a-z-][a-z\d-]*)(?:\.(?:0|[1-9]\d*|\d*[a-z-][a-z\d-]*))*)?(?:\+[a-z\d-]+(?:\.[a-z\d-]+)*)?$/i;
 
-  const isSemVer = (value) => {
-    debug('call:isSemVer(%o)', value);
+const isSemVer = (value) => {
+  debug('call:isSemVer(%o)', value);
 
-    return REGEX.test(value);
-  };
+  return REGEX.test(value);
+};
 
-  // export as commonjs module
-  module.exports = isSemVer;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isSemVer;
