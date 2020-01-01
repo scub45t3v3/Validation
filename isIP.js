fa51e8086988ceb8532e0a9cb3e24ce1801b6146
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isIP.js

--- isIP.js
+++ isIP.js
@@ -1,25 +1,23 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isIP');
-  const net = require('net');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isIP');
+const net = require('net');
 
-  const isIP = (value, version) => {
-    debug('call:isIP(%o, %o)', value, version);
+const isIP = (value, version) => {
+  debug('call:isIP(%o, %o)', value, version);
 
-    switch (version) {
-      case 4:
-      case '4':
-        return net.isIPv4(value);
-      case 6:
-      case '6':
-        return net.isIPv6(value);
-      default:
-        return !!net.isIP(value);
-    }
-  };
+  switch (version) {
+    case 4:
+    case '4':
+      return net.isIPv4(value);
+    case 6:
+    case '6':
+      return net.isIPv6(value);
+    default:
+      return !!net.isIP(value);
+  }
+};
 
-  // export as commonjs module
-  module.exports = isIP;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isIP;
