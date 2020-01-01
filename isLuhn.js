0a26e8660024e7d8c62cfb061684ac1c066aa4dc
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isLuhn.js

--- isLuhn.js
+++ isLuhn.js
@@ -1,45 +1,43 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isLuhn');
-  const REGEX = /^\d+$/;
-  const DOUBLE = [
-    0,
-    2,
-    4,
-    6,
-    8,
-    1,
-    3,
-    5,
-    7,
-    9,
-  ];
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isLuhn');
+const REGEX = /^\d+$/;
+const DOUBLE = [
+  0,
+  2,
+  4,
+  6,
+  8,
+  1,
+  3,
+  5,
+  7,
+  9,
+];
 
-  const isLuhn = (value) => {
-    debug('call:isLuhn(%o)', value);
+const isLuhn = (value) => {
+  debug('call:isLuhn(%o)', value);
 
-    if (!REGEX.test(value)) {
-      return false;
-    }
+  if (!REGEX.test(value)) {
+    return false;
+  }
 
-    const sum = `${value}`
-      .split('')
-      .reverse()
-      .reduce((memo, val, idx) => {
-        val = +val;
+  const sum = `${value}`
+    .split('')
+    .reverse()
+    .reduce((memo, val, idx) => {
+      val = +val;
 
-        if (idx % 2) {
-          return memo + DOUBLE[val];
-        }
+      if (idx % 2) {
+        return memo + DOUBLE[val];
+      }
 
-        return memo + val;
-      }, 0);
+      return memo + val;
+    }, 0);
 
-    return !(sum % 10);
-  };
+  return !(sum % 10);
+};
 
-  // export as commonjs module
-  module.exports = isLuhn;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isLuhn;
