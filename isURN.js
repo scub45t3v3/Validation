7abac5b421c1040f2782ad34aae62c176c9ffb1c
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isURN.js

--- isURN.js
+++ isURN.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isURN');
-  const REGEX = /^urn:([a-z\d][a-z\d-]{0,30}[a-z\d]):((?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@/]|%[\da-fA-F]{2})*)(\?\+(?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@/?]|%[\da-fA-F]{2})*)?(\?=(?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@/?]|%[\da-fA-F]{2})*)?(#(?:[\w.\-~!$&'()*+,;=':@/?]|%[\da-fA-F]{2})*)?$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isURN');
+const REGEX = /^urn:([a-z\d][a-z\d-]{0,30}[a-z\d]):((?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@/]|%[\da-fA-F]{2})*)(\?\+(?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@/?]|%[\da-fA-F]{2})*)?(\?=(?:[\w.\-~!$&'()*+,;=':@]|%[\da-fA-F]{2})+(?:[\w.\-~!$&'()*+,;=':@/?]|%[\da-fA-F]{2})*)?(#(?:[\w.\-~!$&'()*+,;=':@/?]|%[\da-fA-F]{2})*)?$/i;
 
-  const isURN = (value) => {
-    debug('call:isURN(%o)', value);
+const isURN = (value) => {
+  debug('call:isURN(%o)', value);
 
-    return REGEX.test(value);
-  };
+  return REGEX.test(value);
+};
 
-  // export as commonjs module
-  module.exports = isURN;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isURN;
