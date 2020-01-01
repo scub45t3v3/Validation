b70f3be26aca3d31db06cefb21186923c146daf9
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isFrozen.js

--- isFrozen.js
+++ isFrozen.js
@@ -1,15 +1,13 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isFrozen');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isFrozen');
 
-  const isFrozen = (value) => {
-    debug('call:isFrozen(%o)', value);
+const isFrozen = (value) => {
+  debug('call:isFrozen(%o)', value);
 
-    return Object.isFrozen(value);
-  };
+  return Object.isFrozen(value);
+};
 
-  // export as commonjs module
-  module.exports = isFrozen;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isFrozen;
