51f859ead627a7eff28261a0e896737d9ded94d3
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:27:39 +0700

    remove iife from util/GeneratorFunction.js

--- util/GeneratorFunction.js
+++ util/GeneratorFunction.js
@@ -1,6 +1,4 @@
 'use strict';
 
-(() => {
-  // export as commonjs module
-  module.exports = Object.getPrototypeOf(function* () {}).constructor;
-})();
+// export as commonjs module
+module.exports = Object.getPrototypeOf(function* () {}).constructor;
