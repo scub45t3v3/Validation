7fc79b6c21d09de30c844e92ce8f740080efd504
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isPromise.js

--- isPromise.js
+++ isPromise.js
@@ -1,15 +1,13 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isPromise');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isPromise');
 
-  const isPromise = (value) => {
-    debug('call:isPromise(%o)', value);
+const isPromise = (value) => {
+  debug('call:isPromise(%o)', value);
 
-    return value instanceof Promise;
-  };
+  return value instanceof Promise;
+};
 
-  // export as commonjs module
-  module.exports = isPromise;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isPromise;
