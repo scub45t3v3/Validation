c21fcf09ca3dedb02732c949f02a9e06be09cb86
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isSHA384.js

--- isSHA384.js
+++ isSHA384.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isSHA384');
-  const isAll = require('./isAll');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isSHA384');
+const isAll = require('./isAll');
 
-  const isSHA384 = (value) => {
-    debug('call:isSHA384(%o)', value);
+const isSHA384 = (value) => {
+  debug('call:isSHA384(%o)', value);
 
-    return isAll(value, 'isHexadecimal', ['isLength', 96]);
-  };
+  return isAll(value, 'isHexadecimal', ['isLength', 96]);
+};
 
-  // export as commonjs module
-  module.exports = isSHA384;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isSHA384;
