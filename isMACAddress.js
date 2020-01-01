4c4cc78b6199be66c155ccf1b28ba482f655a80b
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isMACAddress.js

--- isMACAddress.js
+++ isMACAddress.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isMACAddress');
-  const REGEX = /^(?:[a-f\d]{2}:){5}[a-f\d]{2}$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isMACAddress');
+const REGEX = /^(?:[a-f\d]{2}:){5}[a-f\d]{2}$/i;
 
-  const isMACAddress = (value) => {
-    debug('call:isMACAddress(%o)', value);
+const isMACAddress = (value) => {
+  debug('call:isMACAddress(%o)', value);
 
-    return REGEX.test(value);
-  };
+  return REGEX.test(value);
+};
 
-  // export as commonjs module
-  module.exports = isMACAddress;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isMACAddress;
