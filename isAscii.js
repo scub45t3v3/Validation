16189088034eec95e344bcf96ae9144630f6ce23
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isAscii.js

--- isAscii.js
+++ isAscii.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isAscii');
-  const REGEX = /^[\x00-\x7F]+$/; // eslint-disable-line no-control-regex
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isAscii');
+const REGEX = /^[\x00-\x7F]+$/; // eslint-disable-line no-control-regex
 
-  const isAscii = (value) => {
-    debug('call:isAscii(%o)', value);
+const isAscii = (value) => {
+  debug('call:isAscii(%o)', value);
 
-    return (value != null) && REGEX.test(value);
-  };
+  return (value != null) && REGEX.test(value);
+};
 
-  // export as commonjs module
-  module.exports = isAscii;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isAscii;
