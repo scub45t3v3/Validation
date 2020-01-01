93e7fdbca46ec39ed4c684e0e35e19d0a14b7a27
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from index.js

--- index.js
+++ index.js
@@ -1,95 +1,93 @@
 'use strict';
 
-(() => {
-  // Aggregate Recursion
-  exports.isAny = require('./isAny');
-  exports.isAll = require('./isAll');
+// Aggregate Recursion
+exports.isAny = require('./isAny');
+exports.isAll = require('./isAll');
 
-  // Type
-  exports.isArray = require('./isArray');
-  exports.isArguments = require('./isArguments');
-  exports.isBoolean = require('./isBoolean');
-  exports.isEmpty = require('./isEmpty');
-  exports.isEqual = require('./isEqual');
-  exports.isError = require('./isError');
-  exports.isFunction = require('./isFunction');
-  exports.isGenerator = require('./isGenerator');
-  exports.isGeneratorFunction = require('./isGeneratorFunction');
-  exports.isIterable = require('./isIterable');
-  exports.isIterableIterator = require('./isIterableIterator');
-  exports.isIterator = require('./isIterator');
-  exports.isMap = require('./isMap');
-  exports.isMatch = require('./isMatch');
-  exports.isNaN = require('./isNaN');
-  exports.isNull = require('./isNull');
-  exports.isPrimitive = require('./isPrimitive');
-  exports.isPromise = require('./isPromise');
-  exports.isRegExp = require('./isRegExp');
-  exports.isSet = require('./isSet');
-  exports.isString = require('./isString');
-  exports.isSymbol = require('./isSymbol');
-  exports.isUndefined = require('./isUndefined');
-  exports.isWeakMap = require('./isWeakMap');
-  exports.isWeakSet = require('./isWeakSet');
+// Type
+exports.isArray = require('./isArray');
+exports.isArguments = require('./isArguments');
+exports.isBoolean = require('./isBoolean');
+exports.isEmpty = require('./isEmpty');
+exports.isEqual = require('./isEqual');
+exports.isError = require('./isError');
+exports.isFunction = require('./isFunction');
+exports.isGenerator = require('./isGenerator');
+exports.isGeneratorFunction = require('./isGeneratorFunction');
+exports.isIterable = require('./isIterable');
+exports.isIterableIterator = require('./isIterableIterator');
+exports.isIterator = require('./isIterator');
+exports.isMap = require('./isMap');
+exports.isMatch = require('./isMatch');
+exports.isNaN = require('./isNaN');
+exports.isNull = require('./isNull');
+exports.isPrimitive = require('./isPrimitive');
+exports.isPromise = require('./isPromise');
+exports.isRegExp = require('./isRegExp');
+exports.isSet = require('./isSet');
+exports.isString = require('./isString');
+exports.isSymbol = require('./isSymbol');
+exports.isUndefined = require('./isUndefined');
+exports.isWeakMap = require('./isWeakMap');
+exports.isWeakSet = require('./isWeakSet');
 
-  // Object
-  exports.isExtensible = require('./isExtensible');
-  exports.isFrozen = require('./isFrozen');
-  exports.isSealed = require('./isSealed');
+// Object
+exports.isExtensible = require('./isExtensible');
+exports.isFrozen = require('./isFrozen');
+exports.isSealed = require('./isSealed');
 
-  // Date
-  exports.isAfter = require('./isAfter');
-  exports.isBefore = require('./isBefore');
-  exports.isDate = require('./isDate');
-  exports.isTime = require('./isTime');
+// Date
+exports.isAfter = require('./isAfter');
+exports.isBefore = require('./isBefore');
+exports.isDate = require('./isDate');
+exports.isTime = require('./isTime');
 
-  // Number
-  exports.isAmericanExpress = require('./isAmericanExpress');
-  exports.isCreditCard = require('./isCreditCard');
-  exports.isDinersClub = require('./isDinersClub');
-  exports.isDiscover = require('./isDiscover');
-  exports.isFloat = require('./isFloat');
-  exports.isHexadecimal = require('./isHexadecimal');
-  exports.isInteger = require('./isInteger');
-  exports.isIP = require('./isIP');
-  exports.isISBN = require('./isISBN');
-  exports.isISIN = require('./isISIN');
-  exports.isISRC = require('./isISRC');
-  exports.isISSN = require('./isISSN');
-  exports.isJCB = require('./isJCB');
-  exports.isLatitude = require('./isLatitude');
-  exports.isLongitude = require('./isLongitude');
-  exports.isLuhn = require('./isLuhn');
-  exports.isMastercard = require('./isMastercard');
-  exports.isPhoneNumber = require('./isPhoneNumber');
-  exports.isPort = require('./isPort');
-  exports.isSemVer = require('./isSemVer');
-  exports.isVIN = require('./isVIN');
-  exports.isVisa = require('./isVisa');
+// Number
+exports.isAmericanExpress = require('./isAmericanExpress');
+exports.isCreditCard = require('./isCreditCard');
+exports.isDinersClub = require('./isDinersClub');
+exports.isDiscover = require('./isDiscover');
+exports.isFloat = require('./isFloat');
+exports.isHexadecimal = require('./isHexadecimal');
+exports.isInteger = require('./isInteger');
+exports.isIP = require('./isIP');
+exports.isISBN = require('./isISBN');
+exports.isISIN = require('./isISIN');
+exports.isISRC = require('./isISRC');
+exports.isISSN = require('./isISSN');
+exports.isJCB = require('./isJCB');
+exports.isLatitude = require('./isLatitude');
+exports.isLongitude = require('./isLongitude');
+exports.isLuhn = require('./isLuhn');
+exports.isMastercard = require('./isMastercard');
+exports.isPhoneNumber = require('./isPhoneNumber');
+exports.isPort = require('./isPort');
+exports.isSemVer = require('./isSemVer');
+exports.isVIN = require('./isVIN');
+exports.isVisa = require('./isVisa');
 
-  // String
-  exports.isAlpha = require('./isAlpha');
-  exports.isAlphaNumeric = require('./isAlphaNumeric');
-  exports.isAscii = require('./isAscii');
-  exports.isBase64 = require('./isBase64');
-  exports.isDataURI = require('./isDataURI');
-  exports.isDomainName = require('./isDomainName');
-  exports.isEmailAddress = require('./isEmailAddress');
-  exports.isHexColor = require('./isHexColor');
-  exports.isJSON = require('./isJSON');
-  exports.isLength = require('./isLength');
-  exports.isMACAddress = require('./isMACAddress');
-  exports.isMD5 = require('./isMD5');
-  exports.isMongoId = require('./isMongoId');
-  exports.isPlural = require('./isPlural');
-  exports.isPostalCode = require('./isPostalCode');
-  exports.isPunctuation = require('./isPunctuation');
-  exports.isSHA1 = require('./isSHA1');
-  exports.isSHA256 = require('./isSHA256');
-  exports.isSHA384 = require('./isSHA384');
-  exports.isSHA512 = require('./isSHA512');
-  exports.isSingular = require('./isSingular');
-  exports.isUUID = require('./isUUID');
-  exports.isURL = require('./isURL');
-  exports.isURN = require('./isURN');
-})(); // end IIFE
+// String
+exports.isAlpha = require('./isAlpha');
+exports.isAlphaNumeric = require('./isAlphaNumeric');
+exports.isAscii = require('./isAscii');
+exports.isBase64 = require('./isBase64');
+exports.isDataURI = require('./isDataURI');
+exports.isDomainName = require('./isDomainName');
+exports.isEmailAddress = require('./isEmailAddress');
+exports.isHexColor = require('./isHexColor');
+exports.isJSON = require('./isJSON');
+exports.isLength = require('./isLength');
+exports.isMACAddress = require('./isMACAddress');
+exports.isMD5 = require('./isMD5');
+exports.isMongoId = require('./isMongoId');
+exports.isPlural = require('./isPlural');
+exports.isPostalCode = require('./isPostalCode');
+exports.isPunctuation = require('./isPunctuation');
+exports.isSHA1 = require('./isSHA1');
+exports.isSHA256 = require('./isSHA256');
+exports.isSHA384 = require('./isSHA384');
+exports.isSHA512 = require('./isSHA512');
+exports.isSingular = require('./isSingular');
+exports.isUUID = require('./isUUID');
+exports.isURL = require('./isURL');
+exports.isURN = require('./isURN');
