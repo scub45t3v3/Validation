2d768bdd7fa78fd5b2bccc18956f94814981b414
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isSHA256.js

--- isSHA256.js
+++ isSHA256.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isSHA256');
-  const isAll = require('./isAll');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isSHA256');
+const isAll = require('./isAll');
 
-  const isSHA256 = (value) => {
-    debug('call:isSHA256(%o)', value);
+const isSHA256 = (value) => {
+  debug('call:isSHA256(%o)', value);
 
-    return isAll(value, 'isHexadecimal', ['isLength', 64]);
-  };
+  return isAll(value, 'isHexadecimal', ['isLength', 64]);
+};
 
-  // export as commonjs module
-  module.exports = isSHA256;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isSHA256;
