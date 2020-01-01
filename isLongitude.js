45b6048c0679f2e0371146b54dfc20b3fd925576
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isLongitude.js

--- isLongitude.js
+++ isLongitude.js
@@ -1,19 +1,17 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isLongitude');
-  const isFloat = require('./isFloat');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isLongitude');
+const isFloat = require('./isFloat');
 
-  const isLongitude = (value) => {
-    debug('call:isLongitude(%o)', value);
+const isLongitude = (value) => {
+  debug('call:isLongitude(%o)', value);
 
-    return isFloat(value, {
-      min: -180,
-      max: 180,
-    });
-  };
+  return isFloat(value, {
+    min: -180,
+    max: 180,
+  });
+};
 
-  // export as commonjs module
-  module.exports = isLongitude;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isLongitude;
