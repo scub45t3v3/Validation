8062079f96c3cfef136c3ebd6bc1a2d4ebf2ab1d
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isAfter.js

--- isAfter.js
+++ isAfter.js
@@ -1,24 +1,22 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isAfter');
-  const moment = require('moment');
-  const isFloat = require('./isFloat');
-  const isDate = require('./isDate');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isAfter');
+const moment = require('moment');
+const isFloat = require('./isFloat');
+const isDate = require('./isDate');
 
-  const isAfter = (value, compare = Date.now()) => {
-    debug('call:isAfter(%o, %o)', value, compare);
+const isAfter = (value, compare = Date.now()) => {
+  debug('call:isAfter(%o, %o)', value, compare);
 
-    if (isFloat(value) && isFloat(compare)) {
-      return parseFloat(value) > parseFloat(compare);
-    } else if (isDate(value)) {
-      return moment(value).isAfter(compare);
-    }
+  if (isFloat(value) && isFloat(compare)) {
+    return parseFloat(value) > parseFloat(compare);
+  } else if (isDate(value)) {
+    return moment(value).isAfter(compare);
+  }
 
-    return false;
-  };
+  return false;
+};
 
-  // export as commonjs module
-  module.exports = isAfter;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isAfter;
