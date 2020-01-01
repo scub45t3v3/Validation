70c09299ece2242a8fe9ba40391e75900eef4951
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isAlphaNumeric.js

--- isAlphaNumeric.js
+++ isAlphaNumeric.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isAlphaNumeric');
-  const REGEX = /^[a-z\d]+$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isAlphaNumeric');
+const REGEX = /^[a-z\d]+$/i;
 
-  const isAlphaNumeric = (value) => {
-    debug('call:isAlphaNumeric(%o)', value);
+const isAlphaNumeric = (value) => {
+  debug('call:isAlphaNumeric(%o)', value);
 
-    return (value != null) && REGEX.test(value);
-  };
+  return (value != null) && REGEX.test(value);
+};
 
-  // export as commonjs module
-  module.exports = isAlphaNumeric;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isAlphaNumeric;
