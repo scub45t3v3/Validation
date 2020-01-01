1df164db4c5f03199592e8882c383a75f3ef8478
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isMongoId.js

--- isMongoId.js
+++ isMongoId.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isMongoId');
-  const isAll = require('./isAll');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isMongoId');
+const isAll = require('./isAll');
 
-  const isMongoId = (value) => {
-    debug('call:isMongoId(%o)', value);
+const isMongoId = (value) => {
+  debug('call:isMongoId(%o)', value);
 
-    return isAll(value, 'isHexadecimal', ['isLength', 24]);
-  };
+  return isAll(value, 'isHexadecimal', ['isLength', 24]);
+};
 
-  // export as commonjs module
-  module.exports = isMongoId;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isMongoId;
