1bfdcf0a79393ba61956264580f686c3ab0ed1cf
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:27:39 +0700

    remove iife from util/toCallable.js

--- util/toCallable.js
+++ util/toCallable.js
@@ -1,41 +1,39 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const _ = require('underscore');
-  const debug = require('debug')('@scuba-squad:validation:util:toCallable');
-  const isFunction = require('../isFunction');
-  const isString = require('../isString');
-  const isRegExp = require('../isRegExp');
-  const isArray = require('../isArray');
+// include dependencies
+const _ = require('underscore');
+const debug = require('debug')('@scuba-squad:validation:util:toCallable');
+const isFunction = require('../isFunction');
+const isString = require('../isString');
+const isRegExp = require('../isRegExp');
+const isArray = require('../isArray');
 
-  const toCallable = (value) => {
-    debug('call:toCallable(%o)', value);
+const toCallable = (value) => {
+  debug('call:toCallable(%o)', value);
 
-    if (isFunction(value)) {
-      return value;
-    }
+  if (isFunction(value)) {
+    return value;
+  }
 
-    if (isString(value)) {
-      return require(`../${value}`);
-    }
+  if (isString(value)) {
+    return require(`../${value}`);
+  }
 
-    if (isRegExp(value)) {
-      return value.test.bind(value);
-    }
+  if (isRegExp(value)) {
+    return value.test.bind(value);
+  }
 
-    if (isArray(value)) {
-      const func = toCallable(_.first(value));
-      const rest = _.rest(value, 1);
+  if (isArray(value)) {
+    const func = toCallable(_.first(value));
+    const rest = _.rest(value, 1);
 
-      return _.partial(func, _, ...rest);
-    }
+    return _.partial(func, _, ...rest);
+  }
 
-    debug('error:value %o can not be converted to a function', value);
+  debug('error:value %o can not be converted to a function', value);
 
-    throw new TypeError(`${value} can not be called`);
-  }; // end toCallable
+  throw new TypeError(`${value} can not be called`);
+}; // end toCallable
 
-  // export as commmonjs module
-  module.exports = toCallable;
-})(); // end IIFE
+// export as commmonjs module
+module.exports = toCallable;
