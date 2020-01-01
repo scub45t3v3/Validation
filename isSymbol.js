3314c0ff2c696123580fa94b3de50d8a32a70570
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isSymbol.js

--- isSymbol.js
+++ isSymbol.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isSymbol');
-  const REGEX = /^Symbol\(.*\)$/;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isSymbol');
+const REGEX = /^Symbol\(.*\)$/;
 
-  const isSymbol = (value) => {
-    debug('call:isSymbol(%o)', value);
+const isSymbol = (value) => {
+  debug('call:isSymbol(%o)', value);
 
-    return typeof value === 'symbol' || REGEX.test(value);
-  };
+  return typeof value === 'symbol' || REGEX.test(value);
+};
 
-  // export as commonjs module
-  module.exports = isSymbol;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isSymbol;
