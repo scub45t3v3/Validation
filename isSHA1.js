29eb630d1f1f6357698ffb4e06c78d6be7cb6157
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isSHA1.js

--- isSHA1.js
+++ isSHA1.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:debug');
-  const isAll = require('./isAll');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:debug');
+const isAll = require('./isAll');
 
-  const isSHA1 = (value) => {
-    debug('call:isSHA1(%o)', value);
+const isSHA1 = (value) => {
+  debug('call:isSHA1(%o)', value);
 
-    return isAll(value, 'isHexadecimal', ['isLength', 40]);
-  };
+  return isAll(value, 'isHexadecimal', ['isLength', 40]);
+};
 
-  // export as commonjs module
-  module.exports = isSHA1;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isSHA1;
