99c86bcbbb451b82922309a4a103dd467f30d402
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isDomainName.js

--- isDomainName.js
+++ isDomainName.js
@@ -1,27 +1,25 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isDomainName');
-  const punycode = require('punycode');
-  const REGEX = /^(?:[a-z\d](?:[a-z\d-]*[a-z\d])?\.)+[a-z][a-z\d-]*[a-z\d]$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isDomainName');
+const punycode = require('punycode');
+const REGEX = /^(?:[a-z\d](?:[a-z\d-]*[a-z\d])?\.)+[a-z][a-z\d-]*[a-z\d]$/i;
 
-  const isDomainName = (value, idn = true) => {
-    debug('call:isDomainName(%o, %o)', value, idn);
+const isDomainName = (value, idn = true) => {
+  debug('call:isDomainName(%o, %o)', value, idn);
 
-    if (idn) {
-      try {
-        value = punycode.toASCII(value);
-      } catch (error) {
-        debug('error:punycode %o', error);
+  if (idn) {
+    try {
+      value = punycode.toASCII(value);
+    } catch (error) {
+      debug('error:punycode %o', error);
 
-        return false;
-      }
+      return false;
     }
+  }
 
-    return REGEX.test(value);
-  };
+  return REGEX.test(value);
+};
 
-  // export as commonjs module
-  module.exports = isDomainName;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isDomainName;
