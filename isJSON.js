bf6aba6ce634baef889d37732c0d0e86b531af14
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isJSON.js

--- isJSON.js
+++ isJSON.js
@@ -1,21 +1,19 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isJSON');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isJSON');
 
-  const isJSON = (value) => {
-    debug('call:isJSON(%o)', value);
+const isJSON = (value) => {
+  debug('call:isJSON(%o)', value);
 
-    try {
-      JSON.parse(value);
+  try {
+    JSON.parse(value);
 
-      return true;
-    } catch (error) {
-      return false;
-    }
-  };
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
 
-  // export as commonjs module
-  module.exports = isJSON;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isJSON;
