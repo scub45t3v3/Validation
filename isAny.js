adbe5659345df49a0ad3b1724f2d253d1d061cb3
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isAny.js

--- isAny.js
+++ isAny.js
@@ -1,23 +1,21 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const _ = require('underscore');
-  const debug = require('debug')('@scuba-squad:validation:isAny');
-  const toCallable = require('./util/toCallable');
+// include dependencies
+const _ = require('underscore');
+const debug = require('debug')('@scuba-squad:validation:isAny');
+const toCallable = require('./util/toCallable');
 
-  const isAny = (value, ...args) => {
-    debug('call:isAny(%o, %o)', value, args);
+const isAny = (value, ...args) => {
+  debug('call:isAny(%o, %o)', value, args);
 
-    args = _.map(args, toCallable);
+  args = _.map(args, toCallable);
 
-    return _.any(args, (func) => {
-      debug('call:%s(%o)', func.name, value);
+  return _.any(args, (func) => {
+    debug('call:%s(%o)', func.name, value);
 
-      return func(value);
-    });
-  };
+    return func(value);
+  });
+};
 
-  // export as commonjs module
-  module.exports = isAny;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isAny;
