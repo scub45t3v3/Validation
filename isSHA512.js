894c2410b95dfd517f347a0219d726b9e05d471b
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isSHA512.js

--- isSHA512.js
+++ isSHA512.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isSHA512');
-  const isAll = require('./isAll');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isSHA512');
+const isAll = require('./isAll');
 
-  const isSHA512 = (value) => {
-    debug('call:isSHA512(%o)', value);
+const isSHA512 = (value) => {
+  debug('call:isSHA512(%o)', value);
 
-    return isAll(value, 'isHexadecimal', ['isLength', 128]);
-  };
+  return isAll(value, 'isHexadecimal', ['isLength', 128]);
+};
 
-  // export as commonjs module
-  module.exports = isSHA512;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isSHA512;
