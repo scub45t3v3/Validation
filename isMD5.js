44293d78665e725010c64fbc83d5b4b977f37591
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isMD5.js

--- isMD5.js
+++ isMD5.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isMD5');
-  const isAll = require('./isAll');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isMD5');
+const isAll = require('./isAll');
 
-  const isMD5 = (value) => {
-    debug('call:isMD5(%o)', value);
+const isMD5 = (value) => {
+  debug('call:isMD5(%o)', value);
 
-    return isAll(value, 'isHexadecimal', ['isLength', 32]);
-  };
+  return isAll(value, 'isHexadecimal', ['isLength', 32]);
+};
 
-  // export as commonjs module
-  module.exports = isMD5;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isMD5;
