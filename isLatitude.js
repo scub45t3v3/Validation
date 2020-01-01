9c84a2f8cdfb51f6016ffde67dcd72f4d8736ac7
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isLatitude.js

--- isLatitude.js
+++ isLatitude.js
@@ -1,19 +1,17 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isLatitude');
-  const isFloat = require('./isFloat');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isLatitude');
+const isFloat = require('./isFloat');
 
-  const isLatitude = (value) => {
-    debug('call:isLatitude(%o)', value);
+const isLatitude = (value) => {
+  debug('call:isLatitude(%o)', value);
 
-    return isFloat(value, {
-      min: -90,
-      max: 90,
-    });
-  };
+  return isFloat(value, {
+    min: -90,
+    max: 90,
+  });
+};
 
-  // export as commonjs module
-  module.exports = isLatitude;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isLatitude;
