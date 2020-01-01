d3849d5ae2ae23f3e6c3464dd8e2a69d69850a59
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isDate.js

--- isDate.js
+++ isDate.js
@@ -1,17 +1,15 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isDate');
-  const moment = require('moment');
-  const isRegExp = require('./isRegExp');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isDate');
+const moment = require('moment');
+const isRegExp = require('./isRegExp');
 
-  const isDate = (value) => {
-    debug('call:isDate(%o)', value);
+const isDate = (value) => {
+  debug('call:isDate(%o)', value);
 
-    return !isRegExp(value) && moment(value).isValid();
-  };
+  return !isRegExp(value) && moment(value).isValid();
+};
 
-  // export as commonjs module
-  module.exports = isDate;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isDate;
