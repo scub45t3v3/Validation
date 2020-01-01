eb9f304058755d5188049e24ed1271368a6d53f3
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isCreditCard.js

--- isCreditCard.js
+++ isCreditCard.js
@@ -1,24 +1,22 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isCreditCard');
-  const isAny = require('./isAny');
-  const CARDS = [
-    'isAmericanExpress',
-    'isDinersClub',
-    'isDiscover',
-    'isJCB',
-    'isMastercard',
-    'isVisa',
-  ];
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isCreditCard');
+const isAny = require('./isAny');
+const CARDS = [
+  'isAmericanExpress',
+  'isDinersClub',
+  'isDiscover',
+  'isJCB',
+  'isMastercard',
+  'isVisa',
+];
 
-  const isCreditCard = (value) => {
-    debug('call:isCreditCard(%o)', value);
+const isCreditCard = (value) => {
+  debug('call:isCreditCard(%o)', value);
 
-    return isAny(value, ...CARDS);
-  };
+  return isAny(value, ...CARDS);
+};
 
-  // export as commonjs module
-  module.exports = isCreditCard;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isCreditCard;
