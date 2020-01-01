51d2f9c357310f4a7333f00b034a04fa4fe26b38
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isBefore.js

--- isBefore.js
+++ isBefore.js
@@ -1,24 +1,22 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isBefore');
-  const moment = require('moment');
-  const isFloat = require('./isFloat');
-  const isDate = require('./isDate');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isBefore');
+const moment = require('moment');
+const isFloat = require('./isFloat');
+const isDate = require('./isDate');
 
-  const isBefore = (value, compare = Date.now()) => {
-    debug('call:isBefore(%o, %o)', value, compare);
+const isBefore = (value, compare = Date.now()) => {
+  debug('call:isBefore(%o, %o)', value, compare);
 
-    if (isFloat(value) && isFloat(compare)) {
-      return parseFloat(value) < parseFloat(compare);
-    } else if (isDate(value)) {
-      return moment(value).isBefore(compare);
-    }
+  if (isFloat(value) && isFloat(compare)) {
+    return parseFloat(value) < parseFloat(compare);
+  } else if (isDate(value)) {
+    return moment(value).isBefore(compare);
+  }
 
-    return false;
-  };
+  return false;
+};
 
-  // export as commonjs module
-  module.exports = isBefore;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isBefore;
