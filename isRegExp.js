61d0366de864deb7d32ebd5d67adfa80b65fe991
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isRegExp.js

--- isRegExp.js
+++ isRegExp.js
@@ -1,15 +1,13 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isRegExp');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isRegExp');
 
-  const isRegExp = (value) => {
-    debug('call:isRegExp(%o)', value);
+const isRegExp = (value) => {
+  debug('call:isRegExp(%o)', value);
 
-    return value instanceof RegExp;
-  };
+  return value instanceof RegExp;
+};
 
-  // export as commonjs module
-  module.exports = isRegExp;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isRegExp;
