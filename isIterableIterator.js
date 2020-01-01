021c5128ee56b9920ffe4c1a4931cb91c7613380
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isIterableIterator.js

--- isIterableIterator.js
+++ isIterableIterator.js
@@ -1,19 +1,17 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isIterableIterator');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isIterableIterator');
 
-  const isIterableIterator = (value) => {
-    debug('call:isIterableIterator(%o)', value);
+const isIterableIterator = (value) => {
+  debug('call:isIterableIterator(%o)', value);
 
-    try {
-      return value[Symbol.iterator]() === value;
-    } catch (error) {
-      return false;
-    }
-  };
+  try {
+    return value[Symbol.iterator]() === value;
+  } catch (error) {
+    return false;
+  }
+};
 
-  // export as commonjs module
-  module.exports = isIterableIterator;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isIterableIterator;
