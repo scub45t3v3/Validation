12769ea978de8888f0ca95a359aa90bef81f6449
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isBase64.js

--- isBase64.js
+++ isBase64.js
@@ -1,20 +1,18 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isBase64');
-  const isRegExp = require('./isRegExp');
-  const REGEX = /^[a-z\d+/]+={0,2}$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isBase64');
+const isRegExp = require('./isRegExp');
+const REGEX = /^[a-z\d+/]+={0,2}$/i;
 
-  const isBase64 = (value) => {
-    debug('call:isBase64(%o)', value);
+const isBase64 = (value) => {
+  debug('call:isBase64(%o)', value);
 
-    return value
-      && !isRegExp(value)
-      && REGEX.test(value)
-      && !(value.length % 4);
-  };
+  return value
+    && !isRegExp(value)
+    && REGEX.test(value)
+    && !(value.length % 4);
+};
 
-  // export as commonjs module
-  module.exports = isBase64;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isBase64;
