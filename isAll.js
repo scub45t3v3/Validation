0339c3f0c3a0b430397a65bbb979e9f5124c2694
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isAll.js

--- isAll.js
+++ isAll.js
@@ -1,23 +1,21 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const _ = require('underscore');
-  const debug = require('debug')('@scuba-squad:validation:isAll');
-  const toCallable = require('./util/toCallable');
+// include dependencies
+const _ = require('underscore');
+const debug = require('debug')('@scuba-squad:validation:isAll');
+const toCallable = require('./util/toCallable');
 
-  const isAll = (value, ...args) => {
-    debug('call:isAll(%o, %o)', value, args);
+const isAll = (value, ...args) => {
+  debug('call:isAll(%o, %o)', value, args);
 
-    args = _.map(args, toCallable);
+  args = _.map(args, toCallable);
 
-    return _.all(args, (func) => {
-      debug('call:%s(%o)', func.name, value);
+  return _.all(args, (func) => {
+    debug('call:%s(%o)', func.name, value);
 
-      return func(value);
-    });
-  };
+    return func(value);
+  });
+};
 
-  // export as commonjs module
-  module.exports = isAll;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isAll;
