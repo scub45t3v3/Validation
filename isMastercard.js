804d9c48da3930925107719150ba0f8399009671
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isMastercard.js

--- isMastercard.js
+++ isMastercard.js
@@ -1,17 +1,15 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isMastercard');
-  const isLuhn = require('./isLuhn');
-  const REGEX = /^(?:5[1-5]\d{2}|222[1-9]|22[3-9]\d|2[3-6]\d{2}|27[01]\d|2720)\d{12}$/;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isMastercard');
+const isLuhn = require('./isLuhn');
+const REGEX = /^(?:5[1-5]\d{2}|222[1-9]|22[3-9]\d|2[3-6]\d{2}|27[01]\d|2720)\d{12}$/;
 
-  const isMastercard = (value) => {
-    debug('call:isMastercard(%o)', value);
+const isMastercard = (value) => {
+  debug('call:isMastercard(%o)', value);
 
-    return REGEX.test(value) && isLuhn(value);
-  };
+  return REGEX.test(value) && isLuhn(value);
+};
 
-  // export as commonjs module
-  module.exports = isMastercard;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isMastercard;
