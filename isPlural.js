75d88ee814e6da15fe58c7c7e9b346b2a5504b2f
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isPlural.js

--- isPlural.js
+++ isPlural.js
@@ -1,17 +1,15 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isPlural');
-  const pluralize = require('pluralize');
-  const isString = require('./isString');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isPlural');
+const pluralize = require('pluralize');
+const isString = require('./isString');
 
-  const isPlural = (value) => {
-    debug('call:isPlural(%o)', value);
+const isPlural = (value) => {
+  debug('call:isPlural(%o)', value);
 
-    return isString(value) && pluralize.isPlural(value);
-  };
+  return isString(value) && pluralize.isPlural(value);
+};
 
-  // export as commonjs module
-  module.exports = isPlural;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isPlural;
