7fdeb8ae4390b0a0155728bed69cb82d82aec02f
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isHexColor.js

--- isHexColor.js
+++ isHexColor.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isHexColor');
-  const REGEX = /^#?(?:[a-f\d]{3}){1,2}$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isHexColor');
+const REGEX = /^#?(?:[a-f\d]{3}){1,2}$/i;
 
-  const isHexColor = (value) => {
-    debug('call:isHexColor(%o)', value);
+const isHexColor = (value) => {
+  debug('call:isHexColor(%o)', value);
 
-    return REGEX.test(value);
-  };
+  return REGEX.test(value);
+};
 
-  // export as commonjs module
-  module.exports = isHexColor;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isHexColor;
