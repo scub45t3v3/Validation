203dde4a7bd4c9797ec9b0a77374416c81056b0b
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isVIN.js

--- isVIN.js
+++ isVIN.js
@@ -1,43 +1,41 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isVIN');
-  const REGEX = /^[a-hj-nprs-z\d]{17}$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isVIN');
+const REGEX = /^[a-hj-nprs-z\d]{17}$/i;
 
-  const isVIN = (value) => {
-    debug('call:isVIN(%o)', value);
+const isVIN = (value) => {
+  debug('call:isVIN(%o)', value);
 
-    if (!REGEX.test(value)) {
-      return false;
-    }
+  if (!REGEX.test(value)) {
+    return false;
+  }
 
-    return getCheckDigit(value) === value[8].toUpperCase();
-  };
+  return getCheckDigit(value) === value[8].toUpperCase();
+};
 
-  const transliterate = (char) => {
-    debug('call:transliterate(%o)', char);
+const transliterate = (char) => {
+  debug('call:transliterate(%o)', char);
 
-    char = char && char.toUpperCase();
+  char = char && char.toUpperCase();
 
-    return '0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ'.indexOf(char) % 10;
-  };
+  return '0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ'.indexOf(char) % 10;
+};
 
-  const getCheckDigit = (value) => {
-    debug('call:getCheckDigit(%o)', value);
+const getCheckDigit = (value) => {
+  debug('call:getCheckDigit(%o)', value);
 
-    const map = '0123456789X';
-    const weights = '8765432X098765432';
-    const sum = value
-      .toUpperCase()
-      .split('')
-      .reduce((memo, char, idx) => {
-        return memo + (transliterate(char) * map.indexOf(weights[idx]));
-      }, 0);
+  const map = '0123456789X';
+  const weights = '8765432X098765432';
+  const sum = value
+    .toUpperCase()
+    .split('')
+    .reduce((memo, char, idx) => {
+      return memo + (transliterate(char) * map.indexOf(weights[idx]));
+    }, 0);
 
-    return map[sum % 11];
-  };
+  return map[sum % 11];
+};
 
-  // export as commonjs module
-  module.exports = isVIN;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isVIN;
