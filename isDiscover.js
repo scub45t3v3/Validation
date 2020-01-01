84408570af21ab5e108a92a9fa6b6ba25898e0a6
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isDiscover.js

--- isDiscover.js
+++ isDiscover.js
@@ -1,17 +1,15 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isDiscover');
-  const isLuhn = require('./isLuhn');
-  const REGEX = /^6(?:011|5\d{2})\d{12}$/;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isDiscover');
+const isLuhn = require('./isLuhn');
+const REGEX = /^6(?:011|5\d{2})\d{12}$/;
 
-  const isDiscover = (value) => {
-    debug('call:isDiscover(%o)', value);
+const isDiscover = (value) => {
+  debug('call:isDiscover(%o)', value);
 
-    return REGEX.test(value) && isLuhn(value);
-  };
+  return REGEX.test(value) && isLuhn(value);
+};
 
-  // export as commonjs module
-  module.exports = isDiscover;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isDiscover;
