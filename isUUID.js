244c3c3689015784bd701ebb17a97d80bd5cf2f3
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isUUID.js

--- isUUID.js
+++ isUUID.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isUUID');
-  const REGEX = /^[a-f\d]{8}(?:-[a-f\d]{4}){3}-[a-f\d]{12}$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isUUID');
+const REGEX = /^[a-f\d]{8}(?:-[a-f\d]{4}){3}-[a-f\d]{12}$/i;
 
-  const isUUID = (value) => {
-    debug('call:isUUID(%o)', value);
+const isUUID = (value) => {
+  debug('call:isUUID(%o)', value);
 
-    return REGEX.test(value);
-  };
+  return REGEX.test(value);
+};
 
-  // export as commonjs module
-  module.exports = isUUID;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isUUID;
