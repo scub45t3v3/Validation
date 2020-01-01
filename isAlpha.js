384aecd2055bde644549eca6d5d66bf04c778095
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isAlpha.js

--- isAlpha.js
+++ isAlpha.js
@@ -1,16 +1,14 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isAlpha');
-  const REGEX = /^[a-z]+$/i;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isAlpha');
+const REGEX = /^[a-z]+$/i;
 
-  const isAlpha = (value) => {
-    debug('call:isAlpha(%o)', value);
+const isAlpha = (value) => {
+  debug('call:isAlpha(%o)', value);
 
-    return (value != null) && REGEX.test(value);
-  };
+  return (value != null) && REGEX.test(value);
+};
 
-  // export as commonjs module
-  module.exports = isAlpha;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isAlpha;
