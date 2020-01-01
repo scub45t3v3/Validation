b1a982a5c114c69484b7ddceb00aaf0577188eba
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isDinersClub.js

--- isDinersClub.js
+++ isDinersClub.js
@@ -1,17 +1,15 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isDinersClub');
-  const isLuhn = require('./isLuhn');
-  const REGEX = /^3(?:0[0-5]|[68]\d)\d{11}$/;
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isDinersClub');
+const isLuhn = require('./isLuhn');
+const REGEX = /^3(?:0[0-5]|[68]\d)\d{11}$/;
 
-  const isDinersClub = (value) => {
-    debug('call:isDinersClub(%o)', value);
+const isDinersClub = (value) => {
+  debug('call:isDinersClub(%o)', value);
 
-    return REGEX.test(value) && isLuhn(value);
-  };
+  return REGEX.test(value) && isLuhn(value);
+};
 
-  // export as commonjs module
-  module.exports = isDinersClub;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isDinersClub;
