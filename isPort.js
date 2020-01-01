566f581dbc9bf14fd7a74af70b96a9cd3a1c1e96
Steve Winberg <stevewin21@gmail.com>
2020-01-01 18:26:49 +0700

    remove iife from isPort.js

--- isPort.js
+++ isPort.js
@@ -1,19 +1,17 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const debug = require('debug')('@scuba-squad:validation:isPort');
-  const isInteger = require('./isInteger');
+// include dependencies
+const debug = require('debug')('@scuba-squad:validation:isPort');
+const isInteger = require('./isInteger');
 
-  const isPort = (value) => {
-    debug('call:isPort(%o)', value);
+const isPort = (value) => {
+  debug('call:isPort(%o)', value);
 
-    return isInteger(value, {
-      min: 1,
-      max: 65535,
-    });
-  };
+  return isInteger(value, {
+    min: 1,
+    max: 65535,
+  });
+};
 
-  // export as commonjs module
-  module.exports = isPort;
-})(); // end IIFE
+// export as commonjs module
+module.exports = isPort;
