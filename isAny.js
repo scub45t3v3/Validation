c36b869645425e2dbc882f849cd939a1df221427
Steve Winberg <stevewin21@gmail.com>
2020-01-04 22:03:22 +0700

    improve preformance of isAny by reducing iteration and removing underscore dependency

--- isAny.js
+++ isAny.js
@@ -1,16 +1,14 @@
 'use strict';
 
 // include dependencies
-const _ = require('underscore');
 const debug = require('debug')('@scuba-squad:validation:isAny');
 const toCallable = require('./util/toCallable');
 
 const isAny = (value, ...args) => {
   debug('call:isAny(%o, %o)', value, args);
-  args = _.map(args, toCallable);
 
-  return _.any(args, (func) => {
-    return func(value);
+  return args.some((func) => {
+    return toCallable(func)(value);
   });
 };
 
