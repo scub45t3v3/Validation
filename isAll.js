13aca98a727352cab08cdd921e992d852acbf20e
Steve Winberg <stevewin21@gmail.com>
2020-01-04 22:03:11 +0700

    improve preformance of isAll by reducing iteration and removing underscore dependency

--- isAll.js
+++ isAll.js
@@ -1,16 +1,14 @@
 'use strict';
 
 // include dependencies
-const _ = require('underscore');
 const debug = require('debug')('@scuba-squad:validation:isAll');
 const toCallable = require('./util/toCallable');
 
 const isAll = (value, ...args) => {
   debug('call:isAll(%o, %o)', value, args);
-  args = _.map(args, toCallable);
 
-  return _.all(args, (func) => {
-    return func(value);
+  return args.every((func) => {
+    return toCallable(func)(value);
   });
 };
 
