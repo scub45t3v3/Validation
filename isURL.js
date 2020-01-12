4c79c7eab4e2eb800725c66596b19c224fe09059
Steve Winberg <stevewin21@gmail.com>
2020-01-12 17:04:43 +0700

    replace isURL regexp with @scuba-squad/url

--- isURL.js
+++ isURL.js
@@ -2,21 +2,13 @@
 
 // include dependencies
 const debug = require('debug')('@scuba-squad:validation:isURL');
-const isDomainName = require('./isDomainName');
-const isIP = require('./isIP');
-const REGEX = /^[a-z][a-z\d+.-]+:\/\/(?:(?:[\w!$&'()*+,;:=~.-]|%[\da-f]{2})+@)?(?<domain>[^\s:[\]/%?#@]+|\[[:.%\w]+\])(?::\d+)?(?:\/(?:[\w!$&'()*+,;:@=~.-]|%[\da-f]{2})+)*\/?(?:\?(?:[\w!$&'()*+,;:@=~?/.-]|%[\da-f]{2})*)?(?:#(?:[\w!$&'()*+,;:@=~?/.-]|%[\da-f]{2})*)?$/iu;
+const URL = require('@scuba-squad/url');
 
 const isURL = (value) => {
   debug('call:isURL(%o)', value);
 
   try {
-    value = REGEX
-      .exec(value)
-      .groups
-      .domain
-      .replace(/^\[|\]$/gu, '');
-
-    return isDomainName(value) || isIP(value);
+    return !!URL(value).host;
   } catch (error) {
     return false;
   }
